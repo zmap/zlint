0ca9e33256cd12c0912bf4b216191569b82b29ac
zhengping12 <wang426@illinois.edu>
2017-03-17 16:13:35 -0500

    fix to sanFqdn lint and 3 new tests

--- lints/lint_ext_san_dnsname_not_fqdn_test.go
+++ lints/lint_ext_san_dnsname_not_fqdn_test.go
@@ -18,8 +18,47 @@ func TestDnsFqdn(t *testing.T) {
 	}
 }
 
+func TestDnsWildcardFqdn(t *testing.T) {
+	inputPath := "../testlint/testCerts/sanDnsWildcard.cer"
+	desEnum := Pass
+	out, _ := Lints["e_ext_san_dnsname_not_fqdn"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != desEnum {
+		t.Error(
+			"For", inputPath, /* input path*/
+			"expected", desEnum, /* The enum you expected */
+			"got", out.Result, /* Actual Result */
+		)
+	}
+}
+
 func TestDnsNotFqdn(t *testing.T) {
 	inputPath := "../testlint/testCerts/sanOtherName.cer"
+	desEnum := Error
+	out, _ := Lints["e_ext_san_dnsname_not_fqdn"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != desEnum {
+		t.Error(
+			"For", inputPath, /* input path*/
+			"expected", desEnum, /* The enum you expected */
+			"got", out.Result, /* Actual Result */
+		)
+	}
+}
+
+func TestDnsWrongWildward(t *testing.T) {
+	inputPath := "../testlint/testCerts/sanDnsWrongWildcard.cer"
+	desEnum := Error
+	out, _ := Lints["e_ext_san_dnsname_not_fqdn"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != desEnum {
+		t.Error(
+			"For", inputPath, /* input path*/
+			"expected", desEnum, /* The enum you expected */
+			"got", out.Result, /* Actual Result */
+		)
+	}
+}
+
+func TestDnsAsterisk(t *testing.T) {
+	inputPath := "../testlint/testCerts/sanDnsAsterisk.cer"
 	desEnum := Error
 	out, _ := Lints["e_ext_san_dnsname_not_fqdn"].ExecuteTest(ReadCertificate(inputPath))
 	if out.Result != desEnum {
--- util/fqdn.go
+++ util/fqdn.go
@@ -7,7 +7,7 @@ import (
 )
 
 func IsFqdn(domain string) bool {
-	RegExp := regexp.MustCompile(`^((([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]))\.){2,}([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z]{2,3})(\.)*$`)
+	RegExp := regexp.MustCompile(`^((([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9])|\*)\.){2,}([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z]{2,3})(\.)*$`)
 	return RegExp.MatchString(domain)
 }
 
