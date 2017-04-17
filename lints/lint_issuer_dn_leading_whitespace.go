2d1ea919cc4920c5454a64a9b6553ad399268341
zhengping <wang426@illinois.edu>
2017-04-17 16:33:58 -0500

    Add DN attribute value space lints (#28)

---
+++ lints/lint_issuer_dn_leading_whitespace.go
@@ -0,0 +1,38 @@
+// lint_issuer_dn_leading_whitespace.go
+
+package lints
+
+import (
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/util"
+)
+
+type IssuerDNLeadingSpace struct{}
+
+func (l *IssuerDNLeadingSpace) Initialize() error {
+	return nil
+}
+
+func (l *IssuerDNLeadingSpace) CheckApplies(c *x509.Certificate) bool {
+	return true
+}
+
+func (l *IssuerDNLeadingSpace) RunTest(c *x509.Certificate) (ResultStruct, error) {
+	leading, _, err := util.CheckRDNSequenceWhiteSpace(c.RawIssuer)
+	if err != nil {
+		return ResultStruct{Result: Fatal}, err
+	}
+	if leading {
+		return ResultStruct{Result: Warn}, nil
+	}
+	return ResultStruct{Result: Pass}, nil
+}
+
+func init() {
+	RegisterLint(&Lint{
+		Name:          "w_issuer_dn_leading_whitespace",
+		Description:   "AttributeValue in issuer RelativeDistinguishedName sequence SHOULD NOT have leading whitespace",
+		Providence:    "aswlabs certlint",
+		EffectiveDate: util.ZeroDate,
+		Test:          &IssuerDNLeadingSpace{}})
+}
---
+++ lints/lint_issuer_dn_trailing_whitespace.go
@@ -0,0 +1,38 @@
+// lint_issuer_dn_trailing_whitespace.go
+
+package lints
+
+import (
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/util"
+)
+
+type IssuerDNTrailingSpace struct{}
+
+func (l *IssuerDNTrailingSpace) Initialize() error {
+	return nil
+}
+
+func (l *IssuerDNTrailingSpace) CheckApplies(c *x509.Certificate) bool {
+	return true
+}
+
+func (l *IssuerDNTrailingSpace) RunTest(c *x509.Certificate) (ResultStruct, error) {
+	_, trailing, err := util.CheckRDNSequenceWhiteSpace(c.RawIssuer)
+	if err != nil {
+		return ResultStruct{Result: Fatal}, err
+	}
+	if trailing {
+		return ResultStruct{Result: Warn}, nil
+	}
+	return ResultStruct{Result: Pass}, nil
+}
+
+func init() {
+	RegisterLint(&Lint{
+		Name:          "w_issuer_dn_trailing_whitespace",
+		Description:   "AttributeValue in issuer RelativeDistinguishedName sequence SHOULD NOT have trailing whitespace",
+		Providence:    "aswlabs certlint",
+		EffectiveDate: util.ZeroDate,
+		Test:          &IssuerDNTrailingSpace{}})
+}
