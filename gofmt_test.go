7592f9eb6bd31c566dc788e6800396a06026cad8
zhengping <wang426@illinois.edu>
2017-04-20 17:02:36 -0500

    Added gofmt test to check all source code

---
+++ gofmt_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"os/exec"
+	"testing"
+)
+
+func TestGofmt(t *testing.T) {
+	cmd := exec.Command("/bin/sh", "-c", "gofmt -l main.go")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Run()
+	if out.String() != "" {
+		t.Error(
+			"main.go not gofmt'ed",
+		)
+	}
+	cmd = exec.Command("/bin/sh", "-c", "gofmt -l lints/*")
+	cmd.Stdout = &out
+	cmd.Run()
+	if out.String() != "" {
+		t.Error(
+			"lints not gofmt'ed",
+		)
+	}
+	cmd = exec.Command("/bin/sh", "-c", "gofmt -l util/*")
+	cmd.Stdout = &out
+	cmd.Run()
+	if out.String() != "" {
+		t.Error(
+			"util not gofmt'ed",
+		)
+	}
+}
