b5288981f7da38b46e10adf0d6a0d2f79803599b
Joseph Dickinson <jddicki2@illinois.edu>
2017-05-02 16:28:42 -0500

    Adding max_length lint for subject common name

---
+++ lints/lint_subject_common_name_max_length_test.go
@@ -0,0 +1,32 @@
+// lint_subject_common_name_max_length_test.go
+package lints
+
+import (
+	"testing"
+)
+
+func TestSubjectCommonNameLengthGood(t *testing.T) {
+	inputPath := "../testlint/testCerts/subjectCommonNameLengthGood.pem"
+	desEnum := Pass
+	out, _ := Lints["e_subject_common_name_max_length"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != desEnum {
+		t.Error(
+			"For", inputPath, /* input path*/
+			"expected", desEnum, /* The enum you expected */
+			"got", out.Result, /* Actual Result */
+		)
+	}
+}
+
+func TestSubjectCommonNameLong(t *testing.T) {
+	inputPath := "../testlint/testCerts/subjectCommonNameLong.pem"
+	desEnum := Error
+	out, _ := Lints["e_subject_common_name_max_length"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != desEnum {
+		t.Error(
+			"For", inputPath, /* input path*/
+			"expected", desEnum, /* The enum you expected */
+			"got", out.Result, /* Actual Result */
+		)
+	}
+}
