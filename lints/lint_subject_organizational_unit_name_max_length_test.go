f6cc4771d265e7c23904f41fe82720a222630c81
Joseph Dickinson <jddicki2@illinois.edu>
2017-05-02 22:41:35 -0500

    added organizational unit max length lint and tests

---
+++ lints/lint_subject_organizational_unit_name_max_length_test.go
@@ -0,0 +1,32 @@
+// lint_subject_organizational_unit_name_max_length_test.go
+package lints
+
+import (
+	"testing"
+)
+
+func TestSubjectOrganizationalUnitNameLengthGood(t *testing.T) {
+	inputPath := "../testlint/testCerts/subjectOrganizationalUnitNameLengthGood.pem"
+	desEnum := Pass
+	out, _ := Lints["e_subject_organizational_unit_name_max_length"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != desEnum {
+		t.Error(
+			"For", inputPath, /* input path*/
+			"expected", desEnum, /* The enum you expected */
+			"got", out.Result, /* Actual Result */
+		)
+	}
+}
+
+func TestSubjectOrganzationalUnitNameLong(t *testing.T) {
+	inputPath := "../testlint/testCerts/subjectOrganizationalUnitNameLong.pem"
+	desEnum := Error
+	out, _ := Lints["e_subject_organizational_unit_name_max_length"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != desEnum {
+		t.Error(
+			"For", inputPath, /* input path*/
+			"expected", desEnum, /* The enum you expected */
+			"got", out.Result, /* Actual Result */
+		)
+	}
+}
