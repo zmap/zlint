1cfc3874bd06f562c735ab1c95fc4a0ba31474d6
Deepak Kumar <kumarde@umich.edu>
2017-06-01 17:43:26 -0500

    update main with requests from dadrian

--- main.go
+++ main.go
@@ -15,29 +15,27 @@ import (
 )
 
 var ( //flags
-	inPath         string
-	outPath        string
-	multi          bool
-	threaded       bool
-	numCertThreads int
-	prettyPrint    bool
-	numProcs       int
-	channelSize    int
+	inPath           string
+	outPath          string
+	numCertThreads   int
+	prettyPrint      bool
+	numProcs         int
+	channelSize      int
+	crashIfParseFail bool
 )
 
 func init() {
-	flag.StringVar(&inPath, "in-file", "", "File path for the input certificate(s).")
-	flag.StringVar(&outPath, "out-file", "-", "File path for the output JSON.")
+	flag.StringVar(&inPath, "input-file", "", "File path for the input certificate(s).")
+	flag.StringVar(&outPath, "output-file", "-", "File path for the output JSON.")
 	flag.BoolVar(&prettyPrint, "list-lints-json", false, "Use this flag to print supported lints in JSON format, one per line")
-	flag.BoolVar(&multi, "multi", false, "Use this flag to specify inserting many certs at once. Certs in this mode must be Base64 encoded DER strings, one per line.")
-	flag.BoolVar(&threaded, "threads", false, "Use this flag to specify that -multi mode runs multi-threaded. This has no effect otherwise.")
 	flag.IntVar(&numCertThreads, "cert-threads", 1, "Use this flag to specify the number of threads in -threads mode.  This has no effect otherwise.")
-	flag.IntVar(&numProcs, "procs", 1, "Use this flag to specify the number of processes to run on.")
+	flag.IntVar(&numProcs, "procs", 0, "Use this flag to specify the number of processes to run on.")
 	flag.IntVar(&channelSize, "channel-size", 10000, "Use this flag to specify the number of values in the buffered channel.")
+	flag.BoolVar(&crashIfParseFail, "crash", false, "Use this flag if you want to crash on parsing failure.")
 	flag.Parse()
 }
 
-func ProcessCertificate(in <-chan string, wg *sync.WaitGroup) {
+func ProcessCertificate(in <-chan string, out chan<- []byte, wg *sync.WaitGroup) {
 	log.Info("Processing certificates...")
 	defer wg.Done()
 	for raw := range in {
@@ -47,14 +45,18 @@ func ProcessCertificate(in <-chan string, wg *sync.WaitGroup) {
 		}
 		parsed, err := x509.ParseCertificate(der)
 		if err != nil { //could not parse
-			//log.Info("Could not parse certificate with error:", err)
+			if crashIfParseFail {
+				log.Fatal("Could not parse certificate with error: ", err)
+			} else {
+				log.Info("Could not parse certificate with error: ", err)
+			}
 		} else { //parsed
 			zlintResult := zlint.ZLintResultTestHandler(parsed)
 			jsonResult, err := json.Marshal(zlintResult.ZLint)
 			if err != nil {
 				log.Fatal("Could not parse JSON.")
 			}
-			fmt.Println(string(jsonResult))
+			out <- jsonResult
 		}
 	}
 }
@@ -76,6 +78,29 @@ func ReadCertificate(out chan<- string, filename string, wg *sync.WaitGroup) {
 	}
 }
 
+func WriteOutput(in <-chan []byte, outputFileName string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var outFile *os.File
+	var err error
+	var writeOutputToFile bool = false
+	if outputFileName != "" && outputFileName != "-" {
+		outFile, err = os.Create(outputFileName)
+		if err != nil {
+			log.Fatal("Unable to create output file: ", err)
+		}
+		defer outFile.Close()
+		writeOutputToFile = true
+	}
+	for json := range in {
+		if writeOutputToFile {
+			outFile.Write(json)
+			outFile.Write([]byte{'\n'})
+		} else {
+			fmt.Println(string(json))
+		}
+	}
+}
+
 func main() {
 	log.SetLevel(log.InfoLevel)
 	runtime.GOMAXPROCS(numProcs)
@@ -85,19 +110,31 @@ func main() {
 		return
 	}
 
-	//Initialize Channel
+	//Initialize Channels
 	certs := make(chan string, channelSize)
+	jsonOut := make(chan []byte, channelSize)
+
 	var readerWG sync.WaitGroup
 	var procWG sync.WaitGroup
+	var writerWG sync.WaitGroup
+
 	readerWG.Add(1)
+	writerWG.Add(1)
+
 	go ReadCertificate(certs, inPath, &readerWG)
+	go WriteOutput(jsonOut, outPath, &writerWG)
+
 	for i := 0; i < numCertThreads; i++ {
 		procWG.Add(1)
-		go ProcessCertificate(certs, &procWG)
+		go ProcessCertificate(certs, jsonOut, &procWG)
 	}
+
 	go func() {
 		readerWG.Wait()
 		close(certs)
 	}()
+
 	procWG.Wait()
+	close(jsonOut)
+	writerWG.Wait()
 }
