5027899e0f6b7b6475711fd778459bb9fbd101b2
Deepak Kumar <kumarde@umich.edu>
2017-08-20 15:23:32 -0500

    update sub cert aia lint to use proper prefix

--- lints/base.go
+++ lints/base.go
@@ -180,7 +180,7 @@ type LintReport struct {
 	WSubCaNameConstraintsNotCritical                     ResultStruct `json:"w_sub_ca_name_constraints_not_critical,omitempty"`
 	ESubCaNoDnsNameConstraints                           ResultStruct `json:"e_sub_ca_no_dns_name_constraints,omitempty"`
 	ESubCaNoIpNameConstraints                            ResultStruct `json:"e_sub_ca_no_ip_name_constraints,omitempty"`
-	ESubCertAiaDoesNotContainIssuingCaUrl                ResultStruct `json:"e_sub_cert_aia_does_not_contain_issuing_ca_url,omitempty"`
+	ESubCertAiaDoesNotContainIssuingCaUrl                ResultStruct `json:"w_sub_cert_aia_does_not_contain_issuing_ca_url,omitempty"`
 	ESubCertAiaDoesNotContainOcspUrl                     ResultStruct `json:"e_sub_cert_aia_does_not_contain_ocsp_url,omitempty"`
 	ESubCertAiaMissing                                   ResultStruct `json:"e_sub_cert_aia_missing,omitempty"`
 	ESubCertCertPolicyEmpty                              ResultStruct `json:"e_sub_cert_cert_policy_empty,omitempty"`
--- lints/lint_sub_cert_aia_does_not_contain_issuing_ca_url.go
+++ lints/lint_sub_cert_aia_does_not_contain_issuing_ca_url.go
@@ -23,7 +23,7 @@ func (l *subCertIssuerUrl) Initialize() error {
 
 func (l *subCertIssuerUrl) CheckApplies(c *x509.Certificate) bool {
 	// Add conditions for application here
-	return !util.IsCACert(c)
+	return util.IsSubscriberCert(c)
 }
 
 func (l *subCertIssuerUrl) RunTest(c *x509.Certificate) (ResultStruct, error) {
@@ -36,7 +36,7 @@ func (l *subCertIssuerUrl) RunTest(c *x509.Certificate) (ResultStruct, error) {
 
 func init() {
 	RegisterLint(&Lint{
-		Name:          "e_sub_cert_aia_does_not_contain_issuing_ca_url",
+		Name:          "w_sub_cert_aia_does_not_contain_issuing_ca_url",
 		Description:   "Subscriber certificates authorityInformationAccess extension should contain the HTTP URL of the issuing CA’s certificate",
 		Provenance:    "CAB: 7.1.2.3",
 		EffectiveDate: util.CABEffectiveDate,
