ed951f2ed2d5985e309b0de3eaa9920aa409d062
Deepak Kumar <kumarde@umich.edu>
2017-08-21 14:32:37 -0500

    add algorithm not supported

---
+++ lints/lint_signature_algorithm_not_supported.go
@@ -0,0 +1,38 @@
+package lints
+
+import (
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/util"
+)
+
+type signatureAlgorithmNotSupported struct {
+	// Internal data here
+}
+
+func (l *signatureAlgorithmNotSupported) Initialize() error {
+	return nil
+}
+
+func (l *signatureAlgorithmNotSupported) CheckApplies(c *x509.Certificate) bool {
+	return true
+}
+
+func (l *signatureAlgorithmNotSupported) RunTest(c *x509.Certificate) (ResultStruct, error) {
+
+	if c.SignatureAlgorithm == x509.SHA1WithRSA || c.SignatureAlgorithm == x509.SHA256WithRSA || c.SignatureAlgorithm == x509.SHA384WithRSA || c.SignatureAlgorithm == x509.SHA512WithRSA || c.SignatureAlgorithm == x509.DSAWithSHA1 || c.SignatureAlgorithm == x509.DSAWithSHA256 || c.SignatureAlgorithm == x509.ECDSAWithSHA1 || c.SignatureAlgorithm == x509.ECDSAWithSHA256 || c.SignatureAlgorithm == x509.ECDSAWithSHA384 || c.SignatureAlgorithm == x509.ECDSAWithSHA512 {
+		return ResultStruct{Result: Pass}, nil
+	} else {
+		return ResultStruct{Result: Error}, nil
+	}
+}
+
+func init() {
+	RegisterLint(&Lint{
+		Name:          "e_signature_algorithm_not_supported",
+		Description:   "Certificates MUST meet the following requirements for algorithm type: SHA-1*, SHA-256, SHA-384, SHA-512",
+		Provenance:    "BRs: 6.1.5",
+		EffectiveDate: util.ZeroDate,
+		Test:          &signatureAlgorithmNotSupported{},
+		updateReport:  func(report *LintReport, result ResultStruct) { report.ESignatureAlgorithmNotSupported = result },
+	})
+}
---
+++ lints/lint_signature_algorithm_not_supported_test.go
@@ -0,0 +1,18 @@
+package lints
+
+import (
+	"testing"
+)
+
+func TestSignatureAlgorithmNotSupported(t *testing.T) {
+	inputPath := "../testlint/testCerts/md5WithRSASignatureAlgorithm.pem"
+	expected := Error
+	out, _ := Lints["e_signature_algorithm_not_supported"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != expected {
+		t.Error(
+			"For", inputPath,
+			"expected", expected,
+			"got", out.Result,
+		)
+	}
+}
