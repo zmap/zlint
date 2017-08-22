1c166a9599a2dbdaaf54f5a021d7655b33efe1ad
Deepak Kumar <kumarde@umich.edu>
2017-08-22 11:14:16 -0500

    update lint with tests

---
+++ lints/lint_sub_ca_eku_valid_fields.go
@@ -0,0 +1,41 @@
+package lints
+
+import (
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/util"
+)
+
+type subCAEKUValidFields struct {
+	// Internal data here
+}
+
+func (l *subCAEKUValidFields) Initialize() error {
+	return nil
+}
+
+func (l *subCAEKUValidFields) CheckApplies(c *x509.Certificate) bool {
+	return util.IsSubCA(c) && util.IsExtInCert(c, util.EkuSynOid)
+}
+
+func (l *subCAEKUValidFields) RunTest(c *x509.Certificate) (ResultStruct, error) {
+	for _, ekuValue := range c.ExtKeyUsage {
+		if ekuValue == x509.ExtKeyUsageServerAuth ||
+			ekuValue == x509.ExtKeyUsageClientAuth {
+			continue
+		} else {
+			return ResultStruct{Result: Error}, nil
+		}
+	}
+	return ResultStruct{Result: Pass}, nil
+}
+
+func init() {
+	RegisterLint(&Lint{
+		Name:          "e_sub_ca_eku_valid_fields",
+		Description:   "Subordinate CA extkeyUsage, either id-kp-serverAuth or id-kp-clientAuth or both values MUST be present.",
+		Provenance:    "CAB: 7.1.2.2",
+		EffectiveDate: util.CABV116Date,
+		Test:          &subCAEKUValidFields{},
+		updateReport:  func(report *LintReport, result ResultStruct) { report.ESubCaEkuValidFields = result },
+	})
+}
---
+++ lints/lint_sub_ca_eku_valid_fields_test.go
@@ -0,0 +1,32 @@
+// lint_sub_ca_name_constraints_not_critical_test.go
+package lints
+
+import (
+	"testing"
+)
+
+func TestSubCAEKUValidFields(t *testing.T) {
+	inputPath := "../testlint/testCerts/subCAEKUValidFields.pem"
+	desEnum := Pass
+	out, _ := Lints["e_sub_ca_eku_valid_fields"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != desEnum {
+		t.Error(
+			"For", inputPath, /* input path*/
+			"expected", desEnum, /* The enum you expected */
+			"got", out.Result, /* Actual Result */
+		)
+	}
+}
+
+func TestSubCAEKUNotValidFields(t *testing.T) {
+	inputPath := "../testlint/testCerts/subCAEKUNotValidFields.pem"
+	desEnum := Error
+	out, _ := Lints["e_sub_ca_eku_valid_fields"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != desEnum {
+		t.Error(
+			"For", inputPath, /* input path*/
+			"expected", desEnum, /* The enum you expected */
+			"got", out.Result, /* Actual Result */
+		)
+	}
+}
