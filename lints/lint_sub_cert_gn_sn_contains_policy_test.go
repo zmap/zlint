57b4ca48e37957baa8220fafe8f8871f6c68c73c
Deepak Kumar <dkumar11@illinois.edu>
2017-08-23 18:34:40 -0500

    add givenName and surname policy check (#103)
    * add givenName and surname policy check
    
    * given name test fix

---
+++ lints/lint_sub_cert_gn_sn_contains_policy_test.go
@@ -0,0 +1,58 @@
+// lint_sub_cert_key_usage_cert_sign_bit_set_test.go
+package lints
+
+import (
+	"testing"
+)
+
+func TestGivenNameCorrectPolicy(t *testing.T) {
+	inputPath := "../testlint/testCerts/givenNameCorrectPolicy.pem"
+	desEnum := Pass
+	out, _ := Lints["e_sub_cert_given_name_surname_contains_correct_policy"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != desEnum {
+		t.Error(
+			"For", inputPath, /* input path*/
+			"expected", desEnum, /* The enum you expected */
+			"got", out.Result, /* Actual Result */
+		)
+	}
+}
+
+func TestSurnameCorrectPolicy(t *testing.T) {
+	inputPath := "../testlint/testCerts/surnameCorrectPolicy.pem"
+	desEnum := Pass
+	out, _ := Lints["e_sub_cert_given_name_surname_contains_correct_policy"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != desEnum {
+		t.Error(
+			"For", inputPath, /* input path*/
+			"expected", desEnum, /* The enum you expected */
+			"got", out.Result, /* Actual Result */
+		)
+	}
+}
+
+func TestGivenNameIncorrectPolicy(t *testing.T) {
+	inputPath := "../testlint/testCerts/givenNameIncorrectPolicy.pem"
+	desEnum := Error
+	out, _ := Lints["e_sub_cert_given_name_surname_contains_correct_policy"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != desEnum {
+		t.Error(
+			"For", inputPath, /* input path*/
+			"expected", desEnum, /* The enum you expected */
+			"got", out.Result, /* Actual Result */
+		)
+	}
+}
+
+func TestSurnameIncorrectPolicy(t *testing.T) {
+	inputPath := "../testlint/testCerts/surnameIncorrectPolicy.pem"
+	desEnum := Error
+	out, _ := Lints["e_sub_cert_given_name_surname_contains_correct_policy"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != desEnum {
+		t.Error(
+			"For", inputPath, /* input path*/
+			"expected", desEnum, /* The enum you expected */
+			"got", out.Result, /* Actual Result */
+		)
+	}
+}
