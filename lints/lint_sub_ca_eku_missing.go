01b70c8eb4b384af7db781f7fdde57355ada935d
Deepak Kumar <dkumar11@illinois.edu>
2017-08-23 18:26:43 -0500

    add subCA EKU missing lint (#102)

---
+++ lints/lint_sub_ca_eku_missing.go
@@ -0,0 +1,38 @@
+package lints
+
+import (
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/util"
+)
+
+type subCAEKUMissing struct {
+	// Internal data here
+}
+
+func (l *subCAEKUMissing) Initialize() error {
+	return nil
+}
+
+func (l *subCAEKUMissing) CheckApplies(c *x509.Certificate) bool {
+	// Add conditions for application here
+	return util.IsSubCA(c)
+}
+
+func (l *subCAEKUMissing) RunTest(c *x509.Certificate) (ResultStruct, error) {
+	if util.IsExtInCert(c, util.EkuSynOid) {
+		return ResultStruct{Result: Pass}, nil
+	} else {
+		return ResultStruct{Result: Error}, nil
+	}
+}
+
+func init() {
+	RegisterLint(&Lint{
+		Name:          "e_sub_ca_eku_missing",
+		Description:   "Subordinate CA certificate MUST have extkeyUsage extension",
+		Provenance:    "BRs: 7.1.5",
+		EffectiveDate: util.CABEffectiveDate,
+		Test:          &subCAEKUMissing{},
+		updateReport:  func(report *LintReport, result ResultStruct) { report.ESubCaEKUMissing = result },
+	})
+}
