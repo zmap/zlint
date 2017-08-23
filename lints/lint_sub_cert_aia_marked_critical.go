2f1a3c601e88277149d890e21207362dae0feb96
Deepak Kumar <dkumar11@illinois.edu>
2017-08-23 18:33:15 -0500

    add AIA lint for subcert (#100)

---
+++ lints/lint_sub_cert_aia_marked_critical.go
@@ -0,0 +1,39 @@
+package lints
+
+import (
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/util"
+)
+
+type subCertAiaMarkedCritical struct {
+	// Internal data here
+}
+
+func (l *subCertAiaMarkedCritical) Initialize() error {
+	return nil
+}
+
+func (l *subCertAiaMarkedCritical) CheckApplies(c *x509.Certificate) bool {
+	// Add conditions for application here
+	return util.IsSubscriberCert(c) && util.IsExtInCert(c, util.AiaOID)
+}
+
+func (l *subCertAiaMarkedCritical) RunTest(c *x509.Certificate) (ResultStruct, error) {
+	e := util.GetExtFromCert(c, util.AiaOID)
+	if e.Critical {
+		return ResultStruct{Result: Error}, nil
+	} else {
+		return ResultStruct{Result: Pass}, nil
+	}
+}
+
+func init() {
+	RegisterLint(&Lint{
+		Name:          "e_sub_cert_aia_marked_critical",
+		Description:   "Subscriber certificates authorityInformationAccess extension MUST NOT be marked critical",
+		Provenance:    "CAB: 7.1.2.3",
+		EffectiveDate: util.CABEffectiveDate,
+		Test:          &subCertAiaMarkedCritical{},
+		updateReport:  func(report *LintReport, result ResultStruct) { report.ESubCertAIAMarkedCritical = result },
+	})
+}
