eecf3e462a97c518c14ae786632a4b038034ea60
Deepak Kumar <kumarde@umich.edu>
2017-08-23 22:13:08 -0500

    remove .

--- lints/lint_sub_cert_locality_name_must_appear.go
+++ lints/lint_sub_cert_locality_name_must_appear.go
@@ -31,7 +31,7 @@ func (l *subCertLocalityNameMustAppear) RunTest(c *x509.Certificate) (ResultStru
 func init() {
 	RegisterLint(&Lint{
 		Name:          "e_sub_cert_locality_name_must_appear",
-		Description:   "Subscriber Certificate: subject:localityName MUST appear if subject:organizationName, subject:givenName, or subject:surname fields are present but the subject:stateOrProvinceName field is absent..",
+		Description:   "Subscriber Certificate: subject:localityName MUST appear if subject:organizationName, subject:givenName, or subject:surname fields are present but the subject:stateOrProvinceName field is absent.",
 		Provenance:    "BRs: 7.1.4.2.2",
 		EffectiveDate: util.CABEffectiveDate,
 		Test:          &subCertLocalityNameMustAppear{},
