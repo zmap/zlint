65a9b1c6a323df056a72913c900be0bacc9a3c23
Deepak Kumar <dkumar11@illinois.edu>
2017-08-23 22:57:27 -0500

    Add province required check. (#107)
    * locality
    
    * update with province prohibited lint

---
+++ lints/lint_sub_cert_province_must_appear.go
@@ -0,0 +1,40 @@
+package lints
+
+import (
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/util"
+)
+
+type subCertProvinceMustAppear struct {
+	// Internal data here
+}
+
+func (l *subCertProvinceMustAppear) Initialize() error {
+	return nil
+}
+
+func (l *subCertProvinceMustAppear) CheckApplies(c *x509.Certificate) bool {
+	return util.IsSubscriberCert(c)
+}
+
+func (l *subCertProvinceMustAppear) RunTest(c *x509.Certificate) (ResultStruct, error) {
+	if len(c.Subject.Organization) > 0 || len(c.Subject.GivenName) > 0 || len(c.Subject.Surname) > 0 {
+		if len(c.Subject.Locality) == 0 {
+			if len(c.Subject.Province) == 0 {
+				return ResultStruct{Result: Error}, nil
+			}
+		}
+	}
+	return ResultStruct{Result: Pass}, nil
+}
+
+func init() {
+	RegisterLint(&Lint{
+		Name:          "e_sub_cert_province_must_appear",
+		Description:   "Subscriber Certificate: subject:stateOrProvinceName MUST appear if the subject:organizationName, subject:givenName, or subject:surname fields are present and subject:localityName is absent.",
+		Provenance:    "BRs: 7.1.4.2.2",
+		EffectiveDate: util.CABEffectiveDate,
+		Test:          &subCertProvinceMustAppear{},
+		updateReport:  func(report *LintReport, result ResultStruct) { report.ESubCertProvinceMustAppear = result },
+	})
+}
