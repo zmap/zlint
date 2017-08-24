c082259e99a4278baa66c5c7b9657f34ed94b808
Deepak Kumar <dkumar11@illinois.edu>
2017-08-24 16:25:30 -0500

    add serial number low entropy check (#112)
    * add low entropy check
    
    * update to warning
    
    * fix tests

---
+++ lints/lint_serial_number_low_entropy_test.go
@@ -0,0 +1,19 @@
+// lint_serial_number_not_positive_test.go
+package lints
+
+import (
+	"testing"
+)
+
+func TestSnLowEntropy(t *testing.T) {
+	inputPath := "../testlint/testCerts/serialNumberLowEntropy.pem"
+	desEnum := Warn
+	out, _ := Lints["w_serial_number_low_entropy"].ExecuteTest(ReadCertificate(inputPath))
+	if out.Result != desEnum {
+		t.Error(
+			"For", inputPath, /* input path*/
+			"expected", desEnum, /* The enum you expected */
+			"got", out.Result, /* Actual Result */
+		)
+	}
+}
