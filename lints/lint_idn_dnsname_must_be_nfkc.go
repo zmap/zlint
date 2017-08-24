7a95c5f99a4e3bdc65302028278e9293550e9677
Deepak Kumar <kumarde@umich.edu>
2017-08-24 16:06:38 -0500

    remove extra comment

--- lints/lint_idn_dnsname_must_be_nfkc.go
+++ lints/lint_idn_dnsname_must_be_nfkc.go
@@ -1,15 +1,3 @@
-// lint_ext_san_dnsname_not_fqdn.go
-/**************************************************************************************************************************
-7.1.4.2.1. Subject Alternative Name Extension
-Certificate Field: extensions:subjectAltName
-Required/Optional:  Required
-Contents:  This extension MUST contain at least one entry.  Each entry MUST be either a dNSName containing
-the Fully‐Qualified Domain Name or an iPAddress containing the IP address of a server.  The CA MUST
-confirm that the Applicant controls the Fully‐Qualified Domain Name or IP address or has been granted the
-right to use it by the Domain Name Registrant or IP address assignee, as appropriate.
-Wildcard FQDNs are permitted.
-**************************************************************************************************************************/
-
 package lints
 
 import (
