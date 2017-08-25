77bb27c31feb35e6e5365b411d48b4741980b4c4
Deepak Kumar <dkumar11@illinois.edu>
2017-08-25 10:03:29 -0500

    Add malformed unicode for IDN check (#115)
    * malformed unicode
    
    * update description

---
+++ lints/lint_idn_dnsname_malformed_unicode.go
@@ -0,0 +1,45 @@
+package lints
+
+import (
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/util"
+	"golang.org/x/net/idna"
+	"strings"
+)
+
+type IDNMalformedUnicode struct {
+	// Internal data here
+}
+
+func (l *IDNMalformedUnicode) Initialize() error {
+	return nil
+}
+
+func (l *IDNMalformedUnicode) CheckApplies(c *x509.Certificate) bool {
+	return util.IsExtInCert(c, util.SubjectAlternateNameOID)
+}
+
+func (l *IDNMalformedUnicode) RunTest(c *x509.Certificate) (ResultStruct, error) {
+	for _, dns := range c.DNSNames {
+		labels := strings.Split(dns, ".")
+		for _, label := range labels {
+			if strings.HasPrefix(label, "xn--") {
+				_, err := idna.ToUnicode(label)
+				if err != nil {
+					return ResultStruct{Result: Error}, nil
+				}
+			}
+		}
+	}
+	return ResultStruct{Result: Pass}, nil
+}
+
+func init() {
+	RegisterLint(&Lint{
+		Name:          "e_international_dns_name_not_unicode",
+		Description:   "Internationalized DNSNames punycode not valid unicode",
+		Provenance:    "RFC 3490",
+		EffectiveDate: util.RFC3490Date,
+		Test:          &IDNMalformedUnicode{},
+	})
+}
