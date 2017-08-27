e1387e715c8bb0d4ab4a09e7f9e5f1583e40d589
Deepak Kumar <kumarde@umich.edu>
2017-08-27 10:00:17 -0500

    add test for left of public suffix

---
+++ lints/lint_dnsname_wildcard_left_of_public_suffix.go
@@ -0,0 +1,54 @@
+package lints
+
+import (
+	"github.com/weppos/publicsuffix-go/publicsuffix"
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/util"
+)
+
+type DNSNameWildcardLeftofPublicSuffix struct {
+	// Internal data here
+}
+
+func (l *DNSNameWildcardLeftofPublicSuffix) Initialize() error {
+	return nil
+}
+
+func (l *DNSNameWildcardLeftofPublicSuffix) CheckApplies(c *x509.Certificate) bool {
+	return util.IsSubscriberCert(c)
+}
+
+func wildcardLeftOfPublicSuffix(domain string) bool {
+	parsedDomain, err := publicsuffix.Parse(domain)
+	if err != nil {
+		return false
+	}
+	if parsedDomain.TRD == "" {
+		if parsedDomain.SLD == "*" {
+			return true
+		}
+	}
+	return false
+}
+
+func (l *DNSNameWildcardLeftofPublicSuffix) RunTest(c *x509.Certificate) (ResultStruct, error) {
+	if wildcardLeftOfPublicSuffix(c.Subject.CommonName) {
+		return ResultStruct{Result: Warn}, nil
+	}
+	for _, dns := range c.DNSNames {
+		if wildcardLeftOfPublicSuffix(dns) {
+			return ResultStruct{Result: Warn}, nil
+		}
+	}
+	return ResultStruct{Result: Pass}, nil
+}
+
+func init() {
+	RegisterLint(&Lint{
+		Name:          "e_dnsname_wildcard_left_of_public_suffix",
+		Description:   "the CA MUST establish and follow a documented procedure[^pubsuffix] that determines if the wildcard character occurs in the first label position to the left of a “registry‐controlled” label or “public suffix”",
+		Provenance:    "BRs: 3.2.2.6",
+		EffectiveDate: util.CABEffectiveDate,
+		Test:          &DNSNameWildcardLeftofPublicSuffix{},
+	})
+}
