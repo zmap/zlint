b1b113cf0c874cbb3cbcf84f571a0a3f86703fb7
Deepak Kumar <kumarde@umich.edu>
2017-08-28 14:33:05 -0500

    add existent check

--- lints/lint_dnsname_underscore_in_sld.go
+++ lints/lint_dnsname_underscore_in_sld.go
@@ -16,7 +16,7 @@ func (l *DNSNameUnderscoreInSLD) Initialize() error {
 }
 
 func (l *DNSNameUnderscoreInSLD) CheckApplies(c *x509.Certificate) bool {
-	return util.IsSubscriberCert(c)
+	return util.IsSubscriberCert(c) && util.DNSNamesExist(c)
 }
 
 func underscoreInSLD(domain string) (bool, ResultStruct) {
