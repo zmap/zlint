ef2a26bddbfbcddf0f5b9d120560b2fecbf7a597
Deepak Kumar <kumarde@umich.edu>
2017-08-28 11:06:44 -0500

    update to subscriber cert

--- lints/lint_dnsname_right_label_valid_tld.go
+++ lints/lint_dnsname_right_label_valid_tld.go
@@ -14,7 +14,7 @@ func (l *DNSNameValidTLD) Initialize() error {
 }
 
 func (l *DNSNameValidTLD) CheckApplies(c *x509.Certificate) bool {
-	return true
+	return util.IsSubscriberCert(c)
 }
 
 func (l *DNSNameValidTLD) RunTest(c *x509.Certificate) (ResultStruct, error) {
