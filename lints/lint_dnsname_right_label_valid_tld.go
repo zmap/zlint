fbdbc89d7d32082d84c232664caddd40c0df1388
Deepak Kumar <dkumar11@illinois.edu>
2017-08-28 16:00:22 -0500

    Update TLD Script (#116)
    * update tld script
    
    * add check for valid tld
    
    * remove SAN extension check
    
    * update to subscriber cert
    
    * add existent check
    
    * add proper cn check

---
+++ lints/lint_dnsname_right_label_valid_tld.go
@@ -0,0 +1,42 @@
+package lints
+
+import (
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/util"
+)
+
+type DNSNameValidTLD struct {
+	// Internal data here
+}
+
+func (l *DNSNameValidTLD) Initialize() error {
+	return nil
+}
+
+func (l *DNSNameValidTLD) CheckApplies(c *x509.Certificate) bool {
+	return util.IsSubscriberCert(c) && util.DNSNamesExist(c)
+}
+
+func (l *DNSNameValidTLD) RunTest(c *x509.Certificate) (ResultStruct, error) {
+	if c.Subject.CommonName != "" {
+		if !util.HasValidTLD(c.Subject.CommonName) {
+			return ResultStruct{Result: Error}, nil
+		}
+	}
+	for _, dns := range c.DNSNames {
+		if !util.HasValidTLD(dns) {
+			return ResultStruct{Result: Error}, nil
+		}
+	}
+	return ResultStruct{Result: Pass}, nil
+}
+
+func init() {
+	RegisterLint(&Lint{
+		Name:          "e_dnsname_not_valid_tld",
+		Description:   "DNSNames must have a valid TLD.",
+		Provenance:    "RFC 5280",
+		EffectiveDate: util.RFC5280Date,
+		Test:          &DNSNameValidTLD{},
+	})
+}
