a8008017a94f0d62570a4bcb97db7c95fe873c5e
Deepak Kumar <kumarde@umich.edu>
2017-08-29 17:41:04 -0500

    add isCA check on raw extension

--- lints/lint_ca_is_ca.go
+++ lints/lint_ca_is_ca.go
@@ -1,6 +1,7 @@
 package lints
 
 import (
+	"github.com/google/certificate-transparency/go/asn1"
 	"github.com/zmap/zcrypto/x509"
 	"github.com/zmap/zlint/util"
 )
@@ -9,17 +10,28 @@ type caIsCA struct {
 	// Internal data here
 }
 
+type basicConstraints struct {
+	IsCA       bool `asn1:"optional"`
+	MaxPathLen int  `asn1:"optional,default:-1"`
+}
+
 func (l *caIsCA) Initialize() error {
 	return nil
 }
 
 func (l *caIsCA) CheckApplies(c *x509.Certificate) bool {
 	// Add conditions for application here
-	return util.IsExtInCert(c, util.KeyUsageOID) && c.KeyUsage&x509.KeyUsageCertSign != 0
+	return util.IsExtInCert(c, util.KeyUsageOID) && c.KeyUsage&x509.KeyUsageCertSign != 0 && util.IsExtInCert(c, util.BasicConstOID)
 }
 
 func (l *caIsCA) RunTest(c *x509.Certificate) (ResultStruct, error) {
-	if c.IsCA == true {
+	e := util.GetExtFromCert(c, util.BasicConstOID)
+	var constraints basicConstraints
+	_, err := asn1.Unmarshal(e.Value, &constraints)
+	if err != nil {
+		return ResultStruct{Result: Fatal}, nil
+	}
+	if constraints.IsCA == true {
 		return ResultStruct{Result: Pass}, nil
 	} else {
 		return ResultStruct{Result: Error}, nil
