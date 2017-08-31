e498b9795ebc62ecec6bcd74962eb0642898b0e0
David Adrian <davidcadrian@gmail.com>
2017-08-30 23:56:55 -0400

    Cleanup DSA

--- lints/lint_dsa_correct_order_in_subgroup.go
+++ lints/lint_dsa_correct_order_in_subgroup.go
@@ -1,47 +1,47 @@
-// lint_dsa_improper_modulus_or_divisor_size.go
-/************************************************
-BRs: 6.1.5
-Certificates MUST meet the following requirements for algorithm type and key size.
-Minimum DSA modulus and divisor size (bits)***: L=2048,	N=224 or L=2048, N=256.
-**As a note, this points to FIPS 186-4 for further clarification**
-************************************************/
-
 package lints
 
 import (
 	"crypto/dsa"
+	"math/big"
+
 	"github.com/zmap/zcrypto/x509"
 	"github.com/zmap/zlint/util"
-	"math/big"
 )
 
-type dsaSubgroup struct {
-	// Internal data here
-}
+type dsaSubgroup struct{}
 
 func (l *dsaSubgroup) Initialize() error {
 	return nil
 }
 
 func (l *dsaSubgroup) CheckApplies(c *x509.Certificate) bool {
-	// Add conditions for application here
-	return c.PublicKeyAlgorithm == x509.DSA
+	if c.PublicKeyAlgorithm != x509.DSA {
+		return false
+	}
+	if _, ok := c.PublicKey.(*dsa.PublicKey); !ok {
+		return false
+	}
+	return true
 }
 
 func (l *dsaSubgroup) RunTest(c *x509.Certificate) (ResultStruct, error) {
-	dsaKey := c.PublicKey.(*dsa.PublicKey)
+	dsaKey, ok := c.PublicKey.(*dsa.PublicKey)
+	if !ok {
+		return ResultStruct{Result: Fatal}, nil
+	}
 	output := big.Int{}
+
+	// Enforce that Y^Q == 1 mod P, e.g. that Order(Y) == Q mod P.
 	output.Exp(dsaKey.Y, dsaKey.Q, dsaKey.P)
 	if output.Cmp(big.NewInt(1)) == 0 {
 		return ResultStruct{Result: Pass}, nil
-	} else {
-		return ResultStruct{Result: Error}, nil
 	}
+	return ResultStruct{Result: Error}, nil
 }
 
 func init() {
 	RegisterLint(&Lint{
-		Name:          "e_dsa_improper_modulus_or_divisor_size",
+		Name:          "e_dsa_correct_order_in_subgroup",
 		Description:   "DSA: Public key value has the unique correct representation in the field, and that the key has the correct order in the subgroup",
 		Provenance:    "BRs: 6.1.6",
 		EffectiveDate: util.CABEffectiveDate,
