a2f68c6cbdd32f94b8e65306fe6a69f957fa30b0
David Adrian <davidcadrian@gmail.com>
2017-09-01 14:44:43 -0400

    Add licenses header to some files.
    Start adding license header until I got tired of it. Will do more later.

--- lints/lint_ca_crl_sign_not_set.go
+++ lints/lint_ca_crl_sign_not_set.go
@@ -1,9 +1,16 @@
-// lint_ca_crl_sign_not_set.go
-/************************************************
-BRs: 7.1.2.1b
-This extension MUST be present and MUST be marked critical. Bit positions for keyCertSign and cRLSign MUST be set.
-If the Root CA Private Key is used for signing OCSP responses, then the digitalSignature bit MUST be set.
-************************************************/
+/*
+ * ZLint Copyright 2017 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 
 package lints
 
@@ -11,6 +18,13 @@ import (
 	"github.com/zmap/zcrypto/x509"
 	"github.com/zmap/zlint/util"
 )
+
+/************************************************
+BRs: 7.1.2.1b
+This extension MUST be present and MUST be marked critical. Bit positions for
+keyCertSign and cRLSign MUST be set. If the Root CA Private Key is used for
+signing OCSP responses, then the digitalSignature bit MUST be set.
+************************************************/
 
 type caCRLSignNotSet struct{}
 
