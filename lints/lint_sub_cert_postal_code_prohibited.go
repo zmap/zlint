3a4b0a76aa3edb34e02cbb689e2e929b5469b018
Deepak Kumar <kumarde@umich.edu>
2017-09-01 10:06:59 -0500

    address ambiguity in BRs

--- lints/lint_sub_cert_postal_code_prohibited.go
+++ lints/lint_sub_cert_postal_code_prohibited.go
@@ -16,7 +16,7 @@ func (l *subCertPostalCodeMustNotAppear) CheckApplies(c *x509.Certificate) bool 
 }
 
 func (l *subCertPostalCodeMustNotAppear) Execute(c *x509.Certificate) *LintResult {
-	if len(c.Subject.Organization) == 0 || len(c.Subject.GivenName) == 0 || len(c.Subject.Surname) == 0 {
+	if len(c.Subject.Organization) == 0 && len(c.Subject.GivenName) == 0 && len(c.Subject.Surname) == 0 {
 		if len(c.Subject.PostalCode) > 0 {
 			return &LintResult{Status: Error}
 		}
