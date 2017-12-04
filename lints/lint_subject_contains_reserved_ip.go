6c4a75f84e594d40a1d88fb02dcd8cfb89330fe8
Maciej Galkowski <maciejgalkowski@gmail.com>
2017-12-04 16:01:33 +0000

    Enhance e_ext_san_contains_reserved_ip linter to check for more reserved IP ranges, reject 0.0.0.0 IP address (#182)

--- lints/lint_subject_contains_reserved_ip.go
+++ lints/lint_subject_contains_reserved_ip.go
@@ -28,15 +28,10 @@ func (l *subjectReservedIP) CheckApplies(c *x509.Certificate) bool {
 }
 
 func (l *subjectReservedIP) Execute(c *x509.Certificate) *LintResult {
-	if ip := net.ParseIP(c.Subject.CommonName); ip != nil {
-		if !util.IsReservedIP(ip) {
-			return &LintResult{Status: Pass}
-		} else {
-			return &LintResult{Status: Error}
-		}
-	} else {
-		return &LintResult{Status: Pass}
+	if ip := net.ParseIP(c.Subject.CommonName); ip != nil && util.IsIANAReserved(ip) {
+		return &LintResult{Status: Error}
 	}
+	return &LintResult{Status: Pass}
 }
 
 func init() {
