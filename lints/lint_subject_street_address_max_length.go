860a701c9b8c1e9648f3bec85531ed09101cebee
Maciej Galkowski <maciejgalkowski@gmail.com>
2017-12-07 18:46:28 +0000

    Add length check linters for the remaining subject DN fields (#185)
    * Add length check linters for the remaining subject DN fields
    
    * Fix typo

---
+++ lints/lint_subject_street_address_max_length.go
@@ -0,0 +1,46 @@
+// lint_subject_street_address_max_length.go
+/************************************************
+ITU-T X.520 (02/2001) UpperBounds
+ub-street-address INTEGER ::= 128
+
+************************************************/
+
+package lints
+
+import (
+	"unicode/utf8"
+
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/util"
+)
+
+type subjectStreetAddressMaxLength struct{}
+
+func (l *subjectStreetAddressMaxLength) Initialize() error {
+	return nil
+}
+
+func (l *subjectStreetAddressMaxLength) CheckApplies(c *x509.Certificate) bool {
+	return true
+}
+
+func (l *subjectStreetAddressMaxLength) Execute(c *x509.Certificate) *LintResult {
+	for _, j := range c.Subject.StreetAddress {
+		if utf8.RuneCountInString(j) > 128 {
+			return &LintResult{Status: Error}
+		}
+	}
+
+	return &LintResult{Status: Pass}
+}
+
+func init() {
+	RegisterLint(&Lint{
+		Name:          "e_subject_street_address_max_length",
+		Description:   "The 'StreetAddress' field of the subject MUST be less than 129 characters",
+		Citation:      "ITU-T X.520 (02/2001) UpperBounds",
+		Source:        RFC5280,
+		EffectiveDate: util.RFC2459Date,
+		Lint:          &subjectStreetAddressMaxLength{},
+	})
+}
