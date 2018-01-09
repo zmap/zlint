e1076634c64d9fd5d0f16244f6c379304d283a78
Zakir Durumeric <zakird@gmail.com>
2018-01-09 18:41:41 +0100

    shortening description to what's tested in lint

--- lints/lint_sub_cert_valid_time_longer_than_825_days.go
+++ lints/lint_sub_cert_valid_time_longer_than_825_days.go
@@ -27,7 +27,7 @@ func (l *subCertValidTimeLongerThan825Days) Execute(c *x509.Certificate) *LintRe
 func init() {
 	RegisterLint(&Lint{
 		Name:          "e_sub_cert_valid_time_longer_than_825_days",
-		Description:   "Subscriber Certificates issued after 1 March 2018 MUST have a Validity Period no greater than 825 days. Subscriber Certificates issued after 1 July 2016 but prior to 1 March 2018 MUST have a Validity Period no greater than 39 months.",
+		Description:   "Subscriber Certificates issued after 1 March 2018 MUST have a Validity Period no greater than 825 days.",
 		Citation:      "BRs: 6.3.2",
 		Source:        CABFBaselineRequirements,
 		EffectiveDate: util.SubCert825Days,
