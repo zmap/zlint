72b5dedab86adac71a6253573805106be3dc86dc
Daniel <daniel@binaryparadox.net>
2019-03-02 10:09:56 -0500

    lints: enforce .onion subjects are in EV certs.
    Adds a lint (`lint_san_dns_name_onion_not_ev_cert.go`) that validates
    that any subscriber certificates containing a `.onion` subject that were
    issued after May 1st, 2015 are EV certificates. Any non-EV certs issued
    after this date that contain a `.onion` subject should receive an
    `Error` lint result.

---
+++ lints/lint_san_dns_name_onion_not_ev_cert.go
@@ -0,0 +1,78 @@
+/*
+ * ZLint Copyright 2019 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package lints
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/util"
+)
+
+const (
+	// onionTLD is a const for the TLD for Tor Hidden Services.
+	onionTLD = ".onion"
+)
+
+type onionNotEV struct{}
+
+// Initialize for an onionNotEV linter is a NOP.
+func (l *onionNotEV) Initialize() error {
+	return nil
+}
+
+// CheckApplies returns true if the certificate is a subscriber certificate.
+func (l *onionNotEV) CheckApplies(c *x509.Certificate) bool {
+	return util.IsSubscriberCert(c)
+}
+
+// Execute checks that if the certificate contains any subject names ending in
+// the  .onion TLD that it is also an EV certificate. If there are .onion names
+// and the certificate is not an EV certificate an Error LintResult is returned.
+func (l *onionNotEV) Execute(c *x509.Certificate) *LintResult {
+	isEV := util.IsEV(c.PolicyIdentifiers)
+	names := append(c.DNSNames, c.Subject.CommonName)
+	for _, name := range names {
+		name = strings.ToLower(name)
+		/*
+		 * Effective May 1, 2015, each CA SHALL revoke all unexpired Certificates with an
+		 * Internal Name using onion as the right-most label in an entry in the
+		 * subjectAltName Extension or commonName field unless such Certificate was
+		 * issued in accordance with Appendix F of the EV Guidelines.
+		 */
+		if strings.HasSuffix(name, onionTLD) && !isEV {
+			return &LintResult{
+				Status: Error,
+				Details: fmt.Sprintf(
+					"certificate contains %s subject domain (%q) but is not an EV certificate",
+					onionTLD, name),
+			}
+		}
+	}
+	return &LintResult{Status: Pass}
+}
+
+func init() {
+	RegisterLint(&Lint{
+		Name:        "san_dns_name_onion_not_ev_cert",
+		Description: "certificates with a .onion subject name must be issued in accordance with EV Guidelines",
+		// TODO(@cpu): Cite section of BRs instead of ballot?
+		Citation:      "BRS: Ballot 144",
+		Source:        CABFBaselineRequirements,
+		EffectiveDate: util.OnionOnlyEVDate,
+		Lint:          &onionNotEV{},
+	})
+}
