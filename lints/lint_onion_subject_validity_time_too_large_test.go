82d49243fb3fde1f0ac62666cfed51821320d398
Daniel <daniel@binaryparadox.net>
2019-03-02 16:28:06 -0500

    lints: enforce .onion cert maximum validity.
    Adds a lint (`lint_onion_subject_validity_time_too_large.go`) that
    validates certificates with one or more `.onion` subjects do not have
    a validity period larger than 15 months.

---
+++ lints/lint_onion_subject_validity_time_too_large_test.go
@@ -0,0 +1,41 @@
+package lints
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTorValidityTooLarge(t *testing.T) {
+	testCases := []struct {
+		Name           string
+		InputFilename  string
+		ExpectedResult LintStatus
+	}{
+		{
+			Name:           "Onion subject, long expiry before util.OnionOnlyEVDate",
+			InputFilename:  "onionSANLongExpiryPreBallot.pem",
+			ExpectedResult: NE,
+		},
+		{
+			Name:           "Onion subject, long expiry, after util.OnionOnlyEVDate",
+			InputFilename:  "onionSANLongExpiry.pem",
+			ExpectedResult: Error,
+		},
+		{
+			Name:           "Onion subject, valid expiry",
+			InputFilename:  "onionSANGoodExpiry.pem",
+			ExpectedResult: Pass,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			inputPath := fmt.Sprintf("%s%s", testCaseDir, tc.InputFilename)
+			result := Lints["onion_subject_validity_time_too_large"].Execute(ReadCertificate(inputPath))
+			if result.Status != tc.ExpectedResult {
+				t.Errorf("expected result %v was %v", tc.ExpectedResult, result.Status)
+			}
+		})
+	}
+
+}
