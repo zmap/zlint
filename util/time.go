c8bc33bb3a1ca9a78d2bdf4c806bc2465a4efdce
Daniel McCarney <daniel@binaryparadox.net>
2019-03-06 17:15:22 -0500

    lints: enforce .onion certs are EV, have <15mo expiry (#265)
    * lints: enforce .onion subjects are in EV certs.
    
    Adds a lint (`lint_san_dns_name_onion_not_ev_cert.go`) that validates
    that any subscriber certificates containing a `.onion` subject that were
    issued after May 1st, 2015 are EV certificates. Any non-EV certs issued
    after this date that contain a `.onion` subject should receive an
    `Error` lint result.
    
    * lints: enforce .onion cert maximum validity.
    
    Adds a lint (`lint_onion_subject_validity_time_too_large.go`) that
    validates certificates with one or more `.onion` subjects do not have
    a validity period larger than 15 months.
    
    * util: add CertificateSubjInTLD helper.
    
    * lints: update .onion lint Citations.
    
    * lints: cleanup duplicate loop in onion EV lint

--- util/time.go
+++ util/time.go
@@ -51,6 +51,7 @@ var (
 	SubCert825Days              = time.Date(2018, time.March, 2, 0, 0, 0, 0, time.UTC)
 	CABV148Date                 = time.Date(2017, time.June, 8, 0, 0, 0, 0, time.UTC)
 	EtsiEn319_412_5_V2_2_1_Date = time.Date(2017, time.November, 1, 0, 0, 0, 0, time.UTC)
+	OnionOnlyEVDate             = time.Date(2015, time.May, 1, 0, 0, 0, 0, time.UTC)
 )
 
 func FindTimeType(firstDate, secondDate asn1.RawValue) (int, int) {
