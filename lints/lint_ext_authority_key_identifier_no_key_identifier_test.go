b52f6183466973aee38952e03062ae0016b792a7
Zakir Durumeric <zakird@gmail.com>
2019-03-10 20:30:04 -0400

    new test for a lint

---
+++ lints/lint_ext_authority_key_identifier_no_key_identifier_test.go
@@ -0,0 +1,37 @@
+package lints
+
+/*
+ * ZLint Copyright 2018 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import (
+	"testing"
+)
+
+func TestAuthorityKeyIdNoKeyIdField(t *testing.T) {
+	inputPath := "../testlint/testCerts/akidNoKeyIdentifier.pem"
+	expected := Error
+	out := Lints["e_ext_authority_key_identifier_no_key_identifier"].Execute(ReadCertificate(inputPath))
+	if out.Status != expected {
+		t.Errorf("%s: expected %s, got %s", inputPath, expected, out.Status)
+	}
+}
+
+func TestAuthorityKeyIdYesKeyIdField(t *testing.T) {
+	inputPath := "../testlint/testCerts/akidWithKeyID.pem"
+	expected := Pass
+	out := Lints["e_ext_authority_key_identifier_no_key_identifier"].Execute(ReadCertificate(inputPath))
+	if out.Status != expected {
+		t.Errorf("%s: expected %s, got %s", inputPath, expected, out.Status)
+	}
+}
