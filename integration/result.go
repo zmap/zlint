227314aaca984cd9137bdf34796b294566e8725f
Daniel McCarney <daniel@binaryparadox.net>
2019-11-24 16:51:13 -0500

    integration: restructure expected data to use lints vs. cert FPs (#329)
    * integration: small TestMain tidy-up chore.
    
    * integration: rename -summary flag to be more specific
    
    * integration: rename -force to -forceDownload.
    
    * integration: add -overwriteExpected flag
    
    * integration: add -lintSummarize, results by lint name.
    
    * integration: rename result type to resultCount
    
    * integration: cleaner resultCount increment
    
    * integration: sort summary output.
    
    * integration: support filtering by fp/lint name
    
    * integration: add small certByFP.sh util script
    
    * makefile: add more integration examples.
    
    * integration: track expected by lint instead of cert fp.
    
    * README: first pass at integration test docs
    
    * integration: show # of files left to download
    
    * CI: remove data caching.
    
    Per Travis docs the cache is fetched over the network from an
    S3-like-service and isn't any faster than downloading the data directly
    from Github for our use-case. Removing the caching saves us from
    spending time at the end of builds uploading the new cache data back to
    the cloud.
    
    * integration: fix progress print format args
    
    * integration: print loading progress
    
    * integration: refine README
    
    * integration: README clarifications.

--- integration/result.go
+++ integration/result.go
@@ -2,28 +2,51 @@
 
 package integration
 
-import "fmt"
+import (
+	"fmt"
 
-type result struct {
+	"github.com/zmap/zlint/lints"
+)
+
+type resultCount struct {
 	FatalCount  uint8 `json:",omitempty"`
 	ErrCount    uint8 `json:",omitempty"`
 	WarnCount   uint8 `json:",omitempty"`
 	NoticeCount uint8 `json:",omitempty"`
 }
 
-func (r result) fullPass() bool {
+// TODO(@cpu): Accept a threshold argument so that (for e.g. notices could be
+// counted as passing)
+func (r resultCount) fullPass() bool {
 	return r.FatalCount == 0 && r.ErrCount == 0 && r.WarnCount == 0 && r.NoticeCount == 0
 }
 
-func (r result) String() string {
-	return fmt.Sprintf("fatals: %d\terrs: %d\twarns: %d\tinfos: %d",
+func (r resultCount) String() string {
+	return fmt.Sprintf("fatals: %-4d errs: %-4d warns: %-4d infos: %-4d",
 		r.FatalCount, r.ErrCount, r.WarnCount, r.NoticeCount)
 }
 
-// certResult combines a Result with a Fingerprint.
+// Inc increases the resultCount count for the given lint status level.
+func (r *resultCount) Inc(status lints.LintStatus) {
+	switch status {
+	case lints.Notice:
+		r.NoticeCount++
+	case lints.Warn:
+		r.WarnCount++
+	case lints.Error:
+		r.ErrCount++
+	case lints.Fatal:
+		r.FatalCount++
+	}
+}
+
+// certResult combines a Result (overall count of lint results by type) with
+// a LintSummary (map from lint name to a Notice/Warn/Error/Fatal result) for
+// a specific cert Fingerprint.
 type certResult struct {
 	Fingerprint string
-	Result      result
+	Result      resultCount
+	LintSummary map[string]lints.LintStatus
 }
 
 func (cr certResult) String() string {
