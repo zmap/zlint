63295c4736b90c6e9778ca4459e961d8c7fc56e0
Daniel <cpu@letsencrypt.org>
2020-01-15 11:18:57 -0500

    lints/mozilla: clarify allowed_eku desc

--- lints/mozilla/lint_mp_allowed_eku.go
+++ lints/mozilla/lint_mp_allowed_eku.go
@@ -67,7 +67,7 @@ func (l *allowedEKU) Execute(c *x509.Certificate) *lint.LintResult {
 func init() {
 	lint.RegisterLint(&lint.Lint{
 		Name:          "n_mp_allowed_eku",
-		Description:   "Separation of id-kp-serverAuth and id-kp-emailProtection KeyPurposeIds",
+		Description:   "A SubCA certificate must not have key usage that allows for both server auth and email protection, and must not use anyKeyUsage",
 		Citation:      "Mozilla Root Store Policy / Section 5.3",
 		Source:        lint.MozillaRootStorePolicy,
 		EffectiveDate: time.Date(2019, time.January, 1, 0, 0, 0, 0, time.UTC),
