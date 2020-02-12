1ae7a79364cb8ca760b445c82db160aba9a53f7d
mtg <git@mtg.de>
2020-02-12 12:18:42 +0100

    implementation of an PSD2 with PSD OI lint

---
+++ lints/etsi/lint_qcstatem_psd2_psd.go
@@ -0,0 +1,78 @@
+package etsi
+
+/*
+ * ZLint Copyright 2020 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import (
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/lint"
+	"github.com/zmap/zlint/util"
+)
+
+type qcStatemPsd2Psd struct{}
+
+func (l *qcStatemPsd2Psd) Initialize() error {
+	return nil
+}
+
+func (l *qcStatemPsd2Psd) CheckApplies(c *x509.Certificate) bool {
+	_, isPresent := util.IsQcStatemPresent(c, &util.IdEtsiPsd2Statem)
+	if !isPresent {
+		return false
+	}
+	if util.CertHasSubjectOrgIdWithPrefix(c, "PSD") {
+		return true
+	}
+	return false
+}
+
+func (l *qcStatemPsd2Psd) Execute(c *x509.Certificate) *lint.LintResult {
+	orgId := util.GetSubjectOrgId(c.RawSubject)
+	if orgId.ErrorString != "" {
+
+		return &lint.LintResult{Status: lint.Error, Details: orgId.ErrorString}
+	}
+	errStr, parsedOi := util.ParseEtsiPsd2OrgId(&orgId.Value)
+	if errStr != "" {
+		return &lint.LintResult{Status: lint.Error, Details: errStr}
+
+	}
+	ext := util.GetExtFromCert(c, util.QcStateOid)
+	s := util.ParseQcStatem(ext.Value, util.IdEtsiPsd2Statem)
+	if s.GetErrorInfo() != "" {
+		return &lint.LintResult{Status: lint.Error, Details: "parsing error for PSD2 QcStatement, cannot properly apply this lint: " + s.GetErrorInfo()}
+	}
+	psd2Statem, ok := s.(util.EtsiPsd2)
+	if !ok {
+		return &lint.LintResult{Status: lint.Fatal, Details: "parsed QcStatem is not of type EtsiPsd2"}
+	}
+	if parsedOi.NcaId != psd2Statem.GetNcaId() {
+		return &lint.LintResult{Status: lint.Error, Details: "NcaId in subject:OrganizationIdentifier and PSD2 QcStatement do not match"}
+	}
+	if parsedOi.Country != psd2Statem.GetNcaCountry() {
+		return &lint.LintResult{Status: lint.Error, Details: "Country in subject:OrganizationIdentifier and PSD2 QcStatement do not match"}
+	}
+	return &lint.LintResult{Status: lint.Pass}
+}
+
+func init() {
+	lint.RegisterLint(&lint.Lint{
+		Name:          "e_qcstatem_psd2_psd",
+		Description:   "Applies to ETSI PSD2 certificates the subject:OrganizationIdentifier of which is of the form 'PSD...' and checks whether the format of the subject:OrganizationIdentifier field is correct and whether the NCAId therein matches the one in the PSD2 statement.",
+		Citation:      "ETSI TS 119 495, '5.2.1 PSD2 Authorization Number or other recognized identifier'",
+		Source:        lint.EtsiEsi,
+		EffectiveDate: util.EtsiPSD2Date,
+		Lint:          &qcStatemPsd2Psd{},
+	})
+}
---
+++ lints/etsi/lint_qcstatem_psd2_psd_test.go
@@ -0,0 +1,77 @@
+package etsi
+
+/*
+ * ZLint Copyright 2018 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import (
+	"github.com/zmap/zlint/lint"
+	"github.com/zmap/zlint/test"
+	"testing"
+)
+
+func TestQcStatemPsd2Psd(t *testing.T) {
+	m := map[string]lint.LintStatus{
+		"QcStmtPsd2Cert01InvalidRoles.pem":                lint.Pass,
+		"QcStmtPsd2Cert02Psd2ExtInvNcaId.pem":             lint.Error,
+		"QcStmtPsd2Cert04Psd2ExtInvNcaId.pem":             lint.Error,
+		"QcStmtPsd2Cert05Valid.pem":                       lint.Pass,
+		"QcStmtPsd2Cert06Psd2ExtWrongNcaId.pem":           lint.Error,
+		"QcStmtPsd2Cert07MissingRoleName.pem":             lint.Error,
+		"QcStmtPsd2Cert08NcaNameMissing.pem":              lint.Error,
+		"QcStmtPsd2Cert09NcaNameZeroLength.pem":           lint.Error,
+		"QcStmtPsd2Cert10RoleNameMissing.pem":             lint.Error,
+		"QcStmtPsd2Cert11RoleNameZeroLenght.pem":          lint.Error,
+		"QcStmtPsd2Cert12NcaIdInconsistent.pem":           lint.Error,
+		"QcStmtPsd2Cert13Psd2ExtNcaIdZeroLength.pem":      lint.Error,
+		"QcStmtPsd2Cert14Valid.pem":                       lint.Pass,
+		"QcStmtPsd2Cert15NcaIdInconsistent.pem":           lint.Error,
+		"QcStmtPsd2Cert16RoleIdAndNameInconsistent.pem":   lint.Pass,
+		"QcStmtPsd2Cert17NcaIdInconsistent.pem":           lint.Error,
+		"QcStmtPsd2Cert18Psd2ExtNcaIdInvalid.pem":         lint.Error,
+		"QcStmtPsd2Cert19ValidTwoRoles.pem":               lint.Pass,
+		"QcStmtPsd2Cert22NcaNameWrongStringType.pem":      lint.Error,
+		"QcStmtPsd2Cert23Psd2ExtNcaIdWrongStringType.pem": lint.Error,
+		"QcStmtPsd2Cert24RoleNameIllegalChars.pem":        lint.Error,
+		"QcStmtPsd2Cert25Valid.pem":                       lint.Pass,
+		"QcStmtPsd2Cert26RoleOidAsUtf8Str.pem":            lint.Error,
+		"QcStmtPsd2Cert27RoleNameNull.pem":                lint.Error,
+		"QcStmtPsd2Cert28NcaNameIa5Str.pem":               lint.Error,
+		"QcStmtPsd2Cert30Valid.pem":                       lint.Pass,
+		"QcStmtPsd2Cert31Valid.pem":                       lint.Pass,
+		"QcStmtPsd2Cert32NcaIdInconsistent.pem":           lint.Error,
+		"QcStmtPsd2Cert34MalformedOrgId.pem":              lint.Error,
+		"QcStmtPsd2Cert35MalformedOrgId.pem":              lint.Error,
+		"QcStmtPsd2Cert36MalformedOrgId.pem":              lint.Error,
+		"QcStmtPsd2Cert37Valid.pem":                       lint.Pass,
+		"QcStmtPsd2Cert38NcaIdLowerCase.pem":              lint.Error,
+		"QcStmtPsd2Cert39Valid.pem":                       lint.Pass,
+		"QcStmtPsd2Cert40Valid.pem":                       lint.NA,
+		"QcStmtPsd2Cert41ValidLei.pem":                    lint.NA,
+		"QcStmtPsd2Cert42OrgIdInvalid.pem":                lint.NA,
+		"QcStmtPsd2Cert43LeiNotXg.pem":                    lint.NA,
+		"QcStmtPsd2Cert44ValidNtr.pem":                    lint.NA,
+		"QcStmtPsd2Cert47MissingUri.pem":                  lint.NA,
+		"QcStmtPsd2Cert48LegalPersonSyntaxViolated.pem":   lint.NA,
+		"QcStmtPsd2Cert50CabfNtrSopSyntaxInPsd2Cert.pem":  lint.NA,
+		"QcStmtPsd2Cert51ValidNoQcs2QcStmt.pem":           lint.NA, // cert has "PDS" not "PSD" prefix
+		"EvAltRegNumCert56JurContryNotMatching.pem":       lint.NA,
+	}
+	for inputPath, expected := range m {
+		out := test.TestLint("e_qcstatem_psd2_psd", inputPath)
+
+		if out.Status != expected {
+			t.Errorf("%s: expected %s, got %s", inputPath, expected, out.Status)
+		}
+	}
+}
