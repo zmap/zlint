53441bdd36c98f9d85ece445effe95d9c283f1c8
Paschalis Korosoglou <824785+pkoro@users.noreply.github.com>
2020-02-13 22:38:04 +0200

    misc: update newLint.sh script and contributing guide.  (#397)
    * Minor changes in lint generator script
    * Modifies package name per new lint
    * Adds variable LINTNAME and seperates it from actual filename

--- newLint.sh
+++ newLint.sh
@@ -1,9 +1,10 @@
 # Script to create new lint from template
 
-USAGE="Usage: $0 <ARG1> <ARG2>
+USAGE="Usage: $0 <ARG1> <ARG2> <ARG3>
 
-ARG1: File_name/TestName (no 'lint_' prefix)
-ARG2: Struct_name"
+ARG1: Path_name
+ARG2: File_name/TestName (no 'lint_' prefix)
+ARG3: Struct_name"
 
 if [ $# -eq 0 ]; then
     echo "No arguments provided..."
@@ -17,18 +18,33 @@ if [ $# -eq 1 ]; then
     exit 1
 fi
 
-if [ -e lint_$1.go ]
+if [ $# -eq 2 ]; then
+    echo "Not enough arguments provided..."
+    echo "$USAGE"
+    exit 1
+fi
+
+if [ ! -d lints/$1 ]
+then
+   echo "Directory 'lints/$1' does not exist. Can't make new file."
+   exit 1
+fi
+
+
+if [ -e lints/$1/lint_$2.go ]
 then
    echo "File already exists. Can't make new file."
    exit 1
 fi
 
-FILENAME=$1
-TESTNAME=$2
+PATHNAME=$1
+LINTNAME=$2
+# Remove the first two characters from ${LINTNAME} and save the resulting string into FILENAME
+FILENAME=${LINTNAME:2}
+STRUCTNAME=$3
 
-cp template lints/lint_$FILENAME.go
+sed -e "s/PACKAGE/${PATHNAME}/" \
+    -e "s/SUBST/${STRUCTNAME}/g" \
+    -e "s/SUBTEST/${LINTNAME}/g" template > lints/${PATHNAME}/lint_${FILENAME}.go
 
-cat "lints/lint_$FILENAME.go" | sed "s/SUBST/$2/g" | sed "s/SUBTEST/$1/g" > temp.go
-mv -f temp.go "lints/lint_$FILENAME.go"
-
-echo "Created file lint_$FILENAME.go with test name $TESTNAME"
+echo "Created file lints/${PATHNAME}/lint_${FILENAME}.go with struct name ${STRUCTNAME}"
