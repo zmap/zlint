fd40f579253ea1ebfb18a585ab5cd8e7dcde61aa
David Adrian <davidcadrian@gmail.com>
2020-02-13 18:34:21 -0500

    Fix v2 with go.mod (#398)
    https://github.com/zmap/zlint/pull/398
    
    Adopt the **Major Subdirectory** approach from
    https://github.com/golang/go/wiki/Modules#releasing-modules-v2-or-higher
    
    ### Effects:
    
    * Moves all code into a `v2` subdirectory. Consumers of ZLint v2 will need to change their imports in go to `github.com/zmap/zlint/v2` and update `go.mod` accordingly.
    * Old versions of ZLint are still fetchable via `go.mod` and version pinning, e.g. `require github.com/zmap/zlint@1.1.0`
    * People using `go get` without modules are going to have all their code break. However, this was going to happen regardless of what we do with our directories because we made breaking changes to our code.
    
    ### Patching old versions
    
    To patch an old version (pre-2.0), we would need to branch off of one of the old tags. To avoid this, we could attempt to maintain support for 1.1.0 by implementing another point release at the top-level directory on top of `v2`, and then exposing the old API's above the v2 directory (see https://github.com/rsc/quote/blob/master/quote.go as an example). However, I don't believe we plan on supporting old versions at all, so there's no reason to do this. On the off chance we do need to cut a point release for 1.1.0, we can use Git.
    
    ### Why not use the Major Branch approach
    
    When not maintaining support for v1 side-by-side with v2, the major branch approach is identical to the Major Subdirectory approach from a Git standpoint---point release for v1 would need to be done on branches. However, it does then have the side effect that the import path for your code no longer matches the directory structure of your code. Effectively, we could `mv` all of the `v2` directory back up to the top-level, claim the name `...v2/`, and use `v2` in all of our import paths. While this might look cleaner from a Git repo standpoint, my gut sense is that we may as well match the directories to the import path, since that's slightly easier to grok.

---
+++ v2/lints/cabf_br/lint_dsa_correct_order_in_subgroup_test.go
@@ -0,0 +1,47 @@
+package cabf_br
+
+/*
+ * ZLint Copyright 2020 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import (
+	"crypto/dsa"
+	"math/big"
+	"testing"
+
+	"github.com/zmap/zlint/v2/lint"
+	"github.com/zmap/zlint/v2/test"
+)
+
+func TestDSACorrectOrderSubgroup(t *testing.T) {
+	inputPath := "dsaCorrectOrderInSubgroup.pem"
+	expected := lint.Pass
+	out := test.TestLint("e_dsa_correct_order_in_subgroup", inputPath)
+	if out.Status != expected {
+		t.Errorf("%s: expected %s, got %s", inputPath, expected, out.Status)
+	}
+}
+
+func TestDSANotCorrectOrderSubgroup(t *testing.T) {
+	inputPath := "dsaCorrectOrderInSubgroup.pem"
+	c := test.ReadTestCert(inputPath)
+	dsaKey := c.PublicKey.(*dsa.PublicKey)
+	pMinusOne := big.NewInt(0)
+	pMinusOne.Sub(dsaKey.P, big.NewInt(1))
+	dsaKey.Y = pMinusOne
+	expected := lint.Error
+	out := test.TestLintCert("e_dsa_correct_order_in_subgroup", c)
+	if out.Status != expected {
+		t.Errorf("%s: expected %s, got %s", inputPath, expected, out.Status)
+	}
+}
