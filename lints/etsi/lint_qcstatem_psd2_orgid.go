1cd0593009b20d54a9b63787208102b2218f5dfa
mtg <git@mtg.de>
2020-02-13 12:17:17 +0100

    implementation of a lint to check correct prefixes in OI

---
+++ lints/etsi/lint_qcstatem_psd2_orgid.go
@@ -0,0 +1,86 @@
+package etsi
+
+/*
+ * ZLint Copyright 2020 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import (
+	"github.com/zmap/zlint/lint"
+	"regexp"
+	"unicode/utf8"
+
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/util"
+)
+
+type qcStatemPsd2OrgId struct{}
+
+func (l *qcStatemPsd2OrgId) Initialize() error {
+	return nil
+}
+
+func (l *qcStatemPsd2OrgId) CheckApplies(c *x509.Certificate) bool {
+	if !util.IsExtInCert(c, util.QcStateOid) {
+		return false
+	}
+	if !util.ParseQcStatem(util.GetExtFromCert(c, util.QcStateOid).Value, util.IdEtsiPsd2Statem).IsPresent() {
+		return false
+	}
+	return true
+}
+
+func orgidHasLegalPersonPrefix(orgId string) bool {
+	if utf8.RuneCountInString(orgId) < 3 {
+		return false
+	}
+	re := regexp.MustCompile(`^(NTR|VAT|PSD|LEI|(.){2}:)`)
+	return re.MatchString(orgId)
+}
+
+func (l *qcStatemPsd2OrgId) Execute(c *x509.Certificate) *lint.LintResult {
+
+	orgId := util.GetSubjectOrgId(c.RawSubject)
+	if len(orgId.ErrorString) != 0 {
+		return &lint.LintResult{Status: lint.Error, Details: orgId.ErrorString}
+	}
+	if !orgId.IsPresent {
+		return &lint.LintResult{Status: lint.Error, Details: "subject:organizationIdentifier field missing from a certificate for which it is mandatory"}
+	}
+
+	_, isPresent := util.IsQcStatemPresent(c, &util.IdQcsPkixQCSyntaxV2)
+	if isPresent {
+		qcs2Generic := util.ParseQcStatem(util.GetQcStatemExtValue(c), util.IdQcsPkixQCSyntaxV2)
+		if qcs2Generic.GetErrorInfo() != "" {
+			return &lint.LintResult{Status: lint.Error, Details: qcs2Generic.GetErrorInfo()}
+		}
+		qc2 := qcs2Generic.(util.DecodedQcS2)
+		if qc2.Decoded.SemanticsId.Equal(util.IdEtsiQcsSemanticsIdLegal) {
+			// TS 119 412 1: all the 5 prefixes allowed
+			if !orgidHasLegalPersonPrefix(orgId.Value) {
+				return &lint.LintResult{Status: lint.Error, Details: "found legal person syntax identifier, but subject:organizationIdentifier does not have one of the prefixes allowed in this syntax"}
+			}
+		}
+	}
+	return &lint.LintResult{Status: lint.Pass}
+}
+
+func init() {
+	lint.RegisterLint(&lint.Lint{
+		Name:          "e_qcstatem_psd2_orgid",
+		Description:   "For a PSD2 Certificate this lint checks that if the Legal Person Semantics Identifier is present, the subject:organizationIdentifier field has one of the allowed prefixes",
+		Citation:      "ETSI TS 119 495 V1.2.1, Sec. 5.2.1",
+		Source:        lint.EtsiEsi,
+		EffectiveDate: util.EtsiPSD2Date,
+		Lint:          &qcStatemPsd2OrgId{},
+	})
+}
---
+++ lints/etsi/lint_qcstatem_psd2_orgid_test.go
@@ -0,0 +1,66 @@
+package etsi
+
+/*
+ * ZLint Copyright 2020 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import (
+	"testing"
+
+	"github.com/zmap/zlint/lint"
+	"github.com/zmap/zlint/test"
+)
+
+func TestQcStatemPsd2OrgIdTestCerts(t *testing.T) {
+	m := map[string]lint.LintStatus{
+		"QcStmtPsd2Cert01InvalidRoles.pem":                lint.Pass,
+		"QcStmtPsd2Cert02Psd2ExtInvNcaId.pem":             lint.Pass,
+		"QcStmtPsd2Cert05Valid.pem":                       lint.Pass,
+		"QcStmtPsd2Cert07MissingRoleName.pem":             lint.Pass,
+		"QcStmtPsd2Cert08NcaNameMissing.pem":              lint.Pass,
+		"QcStmtPsd2Cert09NcaNameZeroLength.pem":           lint.Pass,
+		"QcStmtPsd2Cert10RoleNameMissing.pem":             lint.Pass,
+		"QcStmtPsd2Cert11RoleNameZeroLenght.pem":          lint.Pass,
+		"QcStmtPsd2Cert13Psd2ExtNcaIdZeroLength.pem":      lint.Pass,
+		"QcStmtPsd2Cert14Valid.pem":                       lint.Pass,
+		"QcStmtPsd2Cert16RoleIdAndNameInconsistent.pem":   lint.Pass,
+		"QcStmtPsd2Cert19ValidTwoRoles.pem":               lint.Pass,
+		"QcStmtPsd2Cert20ValidTwoTimesSameRole.pem":       lint.Pass,
+		"QcStmtPsd2Cert21ValidFourRoles.pem":              lint.Pass,
+		"QcStmtPsd2Cert22NcaNameWrongStringType.pem":      lint.Pass,
+		"QcStmtPsd2Cert23Psd2ExtNcaIdWrongStringType.pem": lint.Pass,
+		"QcStmtPsd2Cert24RoleNameIllegalChars.pem":        lint.Pass,
+		"QcStmtPsd2Cert26RoleOidAsUtf8Str.pem":            lint.Pass,
+		"QcStmtPsd2Cert27RoleNameNull.pem":                lint.Pass,
+		"QcStmtPsd2Cert28NcaNameIa5Str.pem":               lint.Pass,
+		"QcStmtPsd2Cert29subjOrgIdMissing.pem":            lint.Error,
+		"QcStmtPsd2Cert30Valid.pem":                       lint.Pass,
+		"QcStmtPsd2Cert31Valid.pem":                       lint.Pass,
+		"QcStmtPsd2Cert33LegalPersonSyntaxViolated.pem":   lint.Error,
+		"QcStmtPsd2Cert41ValidLei.pem":                    lint.Pass,
+		"QcStmtPsd2Cert42OrgIdInvalid.pem":                lint.Pass,
+		"QcStmtPsd2Cert43LeiNotXg.pem":                    lint.Pass,
+		"QcStmtPsd2Cert44ValidNtr.pem":                    lint.Pass,
+		"QcStmtPsd2Cert45LegalPersonSyntaxViolated.pem":   lint.Error,
+		"QcStmtPsd2Cert46InvalidOrgIdNtr.pem":             lint.Pass,
+		"QcStmtPsd2Cert48LegalPersonSyntaxViolated.pem":   lint.Error,
+		"EvAltRegNumCert56JurContryNotMatching.pem":       lint.NA,
+	}
+	for inputPath, expected := range m {
+		out := test.TestLint("e_qcstatem_psd2_orgid", inputPath)
+
+		if out.Status != expected {
+			t.Errorf("%s: expected %s, got %s", inputPath, expected, out.Status)
+		}
+	}
+}
