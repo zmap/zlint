6dc48bb8209bae8bbc3ae5d031373feefcffc648
Rob <3725956+robplee@users.noreply.github.com>
2021-01-11 11:34:58 +0000

    Formatting changes to comment references in rsa_mod_factors_smaller_652_bits and rsa_mod_not_odd
    Co-authored-by: sleevi <ryan.sleevi@gmail.com>

--- v3/lints/cabf_br/lint_rsa_mod_not_odd.go
+++ v3/lints/cabf_br/lint_rsa_mod_not_odd.go
@@ -27,7 +27,7 @@ type rsaParsedTestsKeyModOdd struct{}
 
 /*******************************************************************************************************
 "BRs: 6.1.6"
-RSA: The CA SHALL confirm that the value of the public exponent is an odd number equal to 3 or	more. Additionally,	the public exponent SHOULD be in the range between 2^16+1 and 2^256-1. The modulus SHOULD also have the following characteristics: an odd number, not the power of a prime, and have no factors smaller than 752. [Citation: Section 5.3.3, NIST SP 800-89].
+RSA: The CA SHALL confirm that the value of the public exponent is an odd number equal to 3 or more. Additionally, the public exponent SHOULD be in the range between 2^16+1 and 2^256-1. The modulus SHOULD also have the following characteristics: an odd number, not the power of a prime, and have no factors smaller than 752. [Citation: Section 5.3.3, NIST SP 800‐89].
 *******************************************************************************************************/
 
 func init() {
