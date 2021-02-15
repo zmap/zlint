835500b231bc6214ae534cd8e703ca54e8ead959
Christopher Henderson <chris@chenderson.org>
2021-02-15 10:58:49 -0800

    Custom static analysis tooling for CI/CD (#551)
    * adding custom linters to the code base for static analysis
    
    * renaming main test directory to make more consistent
    
    * Update v3/integration/lints/main.go
    
    Co-authored-by: Daniel McCarney <daniel@binaryparadox.net>
    
    * Update v3/integration/lints/lint/lint.go
    
    Co-authored-by: Daniel McCarney <daniel@binaryparadox.net>
    
    * Update v3/integration/lints/lint/lint.go
    
    Co-authored-by: Daniel McCarney <daniel@binaryparadox.net>
    
    * Update v3/integration/lints/lint/lint.go
    
    Co-authored-by: Daniel McCarney <daniel@binaryparadox.net>
    
    * Update v3/integration/lints/main.go
    
    * Update v3/integration/lints/filters/nodes.go
    
    Co-authored-by: Daniel McCarney <daniel@binaryparadox.net>
    
    * Update v3/integration/lints/filters/nodes.go
    
    Co-authored-by: Daniel McCarney <daniel@binaryparadox.net>
    
    * Update makefile
    
    * Update makefile
    
    * Update v3/integration/lints/lint/lint.go
    
    Co-authored-by: Daniel McCarney <daniel@binaryparadox.net>
    
    Co-authored-by: Daniel McCarney <daniel@binaryparadox.net>

---
+++ v3/integration/lints/lints/init_first_test.go
@@ -0,0 +1,49 @@
+package lints
+
+/*
+ * ZLint Copyright 2021 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import (
+	"testing"
+
+	"github.com/zmap/zlint/v3/integration/lints/lint"
+)
+
+func TestInitFirst_Lint(t *testing.T) {
+	data := []struct {
+		inputFile  string
+		expectPass bool
+	}{
+		{inputFile: "testdata/lint_initializeFirst.go", expectPass: true},
+		{inputFile: "testdata/lint_initializeFirst.go", expectPass: true},
+		{inputFile: "testdata/lint_initializeNotFirst.go", expectPass: false},
+		{inputFile: "testdata/lint_initializeFirstNoFunctions.go", expectPass: false},
+	}
+	l := &InitFirst{}
+	for _, test := range data {
+		file := test.inputFile
+		want := test.expectPass
+		t.Run(file, func(t *testing.T) {
+			r, err := lint.RunLintForFile(file, l)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if want && r != nil {
+				t.Errorf("got unexepcted error result, %s", r)
+			} else if !want && r == nil {
+				t.Errorf("expected failure but got nothing")
+			}
+		})
+	}
+}
