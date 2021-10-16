c1b2ac16aee4d39032e1baeb22f1c42529eab02e
christopher-henderson <chris@chenderson.org>
2021-10-16 13:06:51 -0700

    adding lint to forbid wildcard certs for non .onion EVs

---
+++ v3/lints/cabf_ev/lint_ev_not_wildcard_test.go
@@ -0,0 +1,39 @@
+/*
+ * ZLint Copyright 2021 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package cabf_ev
+
+import (
+	"testing"
+
+	"github.com/zmap/zlint/v3/lint"
+	"github.com/zmap/zlint/v3/test"
+)
+
+func TestSmoke(t *testing.T) {
+	var tests = map[string]lint.LintStatus{
+		"evWildcard.pem":                lint.Error,
+		"evSubscriberNotWildCard.pem":   lint.Pass,
+		"evSubscriberWildcardOnion.pem": lint.Pass,
+	}
+	for file, want := range tests {
+		t.Run(file, func(t *testing.T) {
+			t.Parallel()
+			got := test.TestLint("e_ev_not_wildcard", file).Status
+			if got != want {
+				t.Errorf("want %s, got %s", want, got)
+			}
+		})
+	}
+}
