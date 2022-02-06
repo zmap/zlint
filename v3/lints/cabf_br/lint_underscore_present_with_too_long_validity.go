e77ba1201640bc0d14c8e892ab8b7eab8af8fa4d
christopher-henderson <chris@chenderson.org>
2022-02-06 11:51:16 -0800

    less confusing varname

--- v3/lints/cabf_br/lint_underscore_present_with_too_long_validity.go
+++ v3/lints/cabf_br/lint_underscore_present_with_too_long_validity.go
@@ -31,7 +31,7 @@ func init() {
 		Citation:        "BR 7.1.4.2.1",
 		Source:          lint.CABFBaselineRequirements,
 		EffectiveDate:   util.CABFBRs_1_6_2_Date,
-		IneffectiveDate: util.BALLOT_SC_12_Effective,
+		IneffectiveDate: util.BALLOT_SC_12_Ineffective,
 		Lint:            func() lint.LintInterface { return &UnderscorePresentWithTooLongValidity{} },
 	})
 }
--- v3/util/time.go
+++ v3/util/time.go
@@ -60,7 +60,7 @@ var (
 	MozillaPolicy24Date         = time.Date(2017, time.February, 28, 0, 0, 0, 0, time.UTC)
 	MozillaPolicy241Date        = time.Date(2017, time.March, 31, 0, 0, 0, 0, time.UTC)
 	MozillaPolicy27Date         = time.Date(2020, time.January, 1, 0, 0, 0, 0, time.UTC)
-	BALLOT_SC_12_Effective      = time.Date(2019, time.April, 1, 0, 0, 0, 0, time.UTC)
+	BALLOT_SC_12_Ineffective    = time.Date(2019, time.April, 1, 0, 0, 0, 0, time.UTC)
 	CABFBRs_1_2_1_Date          = time.Date(2015, time.January, 16, 0, 0, 0, 0, time.UTC)
 	CABFBRs_1_6_9_Date          = time.Date(2020, time.March, 27, 0, 0, 0, 0, time.UTC)
 	CABFBRs_1_6_2_Date          = time.Date(2018, time.December, 10, 0, 0, 0, 0, time.UTC)
