6c279f55802761b3eaf1bb11389ac25ef63b4a58
BJ Cardon <cardonator@users.noreply.github.com>
2023-09-11 22:30:41 -0600

    Updated lint for common name handling. The definition for the CN field has switched from deprecated to NOT RECOMMENDED (essentially SHOULD NOT). An IneffectiveDate was added to the original lint.
    Added a new lint for subscriber cert basic constraints checking. Post-SC62, basicConstraint MAY be included but MUST be critical if present.
    
    Added a date for SC62 Effective

--- v3/lints/cabf_br/lint_subject_common_name_included_sc62.go
+++ v3/lints/cabf_br/lint_subject_common_name_included_sc62.go
@@ -20,37 +20,36 @@ import (
 	"github.com/zmap/zlint/v3/util"
 )
 
-type commonNames struct{}
+type commonNamesSC62 struct{}
 
 /***************************************************************
-BRs: 7.1.4.2.2
-Required/Optional: Deprecated (Discouraged, but not prohibited)
+BRs: 7.1.2.7.1
+Required/Optional: NOT RECOMMENDED
 ***************************************************************/
 
 func init() {
 	lint.RegisterLint(&lint.Lint{
-		Name:            "n_subject_common_name_included",
-		Description:     "Subscriber Certificate: commonName is deprecated.",
-		Citation:        "BRs: 7.1.4.2.2",
-		Source:          lint.CABFBaselineRequirements,
-		EffectiveDate:   util.CABEffectiveDate,
-		IneffectiveDate: util.SC62EffectiveDate,
-		Lint:            NewCommonNames,
+		Name:          "w_subject_common_name_included",
+		Description:   "Subscriber Certificate: commonName is NOT RECOMMENDED.",
+		Citation:      "BRs: 7.1.2.7.1",
+		Source:        lint.CABFBaselineRequirements,
+		EffectiveDate: util.SC62EffectiveDate,
+		Lint:          NewCommonNamesSC62,
 	})
 }
 
-func NewCommonNames() lint.LintInterface {
-	return &commonNames{}
+func NewCommonNamesSC62() lint.LintInterface {
+	return &commonNamesSC62{}
 }
 
-func (l *commonNames) CheckApplies(c *x509.Certificate) bool {
+func (l *commonNamesSC62) CheckApplies(c *x509.Certificate) bool {
 	return !util.IsCACert(c)
 }
 
-func (l *commonNames) Execute(c *x509.Certificate) *lint.LintResult {
+func (l *commonNamesSC62) Execute(c *x509.Certificate) *lint.LintResult {
 	if c.Subject.CommonName == "" {
 		return &lint.LintResult{Status: lint.Pass}
 	} else {
-		return &lint.LintResult{Status: lint.Notice}
+		return &lint.LintResult{Status: lint.Warn}
 	}
 }
