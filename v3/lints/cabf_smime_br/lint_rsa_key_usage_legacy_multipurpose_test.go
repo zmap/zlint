a23fb9cc032c8bc687bd4eaaefd960ae52072b1f
robplee <robertplee@live.com>
2023-10-26 15:27:20 +0100

    rename rsa strict ku lint test data to reflect strictness of SMIME policy oid

--- v3/lints/cabf_smime_br/lint_rsa_key_usage_legacy_multipurpose_test.go
+++ v3/lints/cabf_smime_br/lint_rsa_key_usage_legacy_multipurpose_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/zmap/zlint/v3/test"
 )
 
-func TestRSAKeyUsageStrict(t *testing.T) {
+func TestRSAKeyUsageLegacyMultipurpose(t *testing.T) {
 	testCases := []struct {
 		Name           string
 		InputFilename  string
@@ -15,22 +15,22 @@ func TestRSAKeyUsageStrict(t *testing.T) {
 	}{
 		{
 			Name:           "pass - cert with digitalSignature KU",
-			InputFilename:  "smime/rsa_strict_digital_signature_ku.pem",
+			InputFilename:  "smime/rsa_digital_signature_ku.pem",
 			ExpectedResult: lint.Pass,
 		},
 		{
 			Name:           "pass - cert with digitalSignature and contentCommitment KUs",
-			InputFilename:  "smime/rsa_strict_digital_signature_content_commitment_ku.pem",
+			InputFilename:  "smime/rsa_digital_signature_content_commitment_ku.pem",
 			ExpectedResult: lint.Pass,
 		},
 		{
 			Name:           "pass - cert with keyEncipherment KU",
-			InputFilename:  "smime/rsa_strict_key_encipherment_ku.pem",
+			InputFilename:  "smime/rsa_key_encipherment_ku.pem",
 			ExpectedResult: lint.Pass,
 		},
 		{
 			Name:           "pass - cert with digitalSignature, keyEncipherment, and contentCommitment KUs",
-			InputFilename:  "smime/rsa_strict_digital_signature_key_encipherment_content_commitment_ku.pem",
+			InputFilename:  "smime/rsa_digital_signature_key_encipherment_content_commitment_ku.pem",
 			ExpectedResult: lint.Pass,
 		},
 		{
@@ -40,33 +40,33 @@ func TestRSAKeyUsageStrict(t *testing.T) {
 		},
 		{
 			Name:           "NE - certificate with valid KUs dated before 2020-09-01",
-			InputFilename:  "smime/rsa_strict_valid_ku_august_2023.pem",
+			InputFilename:  "smime/rsa_valid_ku_august_2023.pem",
 			ExpectedResult: lint.NE,
 		},
 		{
 			Name:           "Error - Signing Certificate with unexpected KU",
-			InputFilename:  "smime/rsa_strict_digital_signature_cert_sign_ku.pem",
+			InputFilename:  "smime/rsa_digital_signature_cert_sign_ku.pem",
 			ExpectedResult: lint.Error,
 		},
 		{
 			Name:           "Error - Key Management Certificate with unexpected KU",
-			InputFilename:  "smime/rsa_strict_key_encipherment_cert_sign_ku.pem",
+			InputFilename:  "smime/rsa_key_encipherment_cert_sign_ku.pem",
 			ExpectedResult: lint.Error,
 		},
 		{
 			Name:           "Error - Dual Use Certificate with unexpected KU",
-			InputFilename:  "smime/rsa_strict_digital_signature_key_encipherment_cert_sign_ku.pem",
+			InputFilename:  "smime/rsa_digital_signature_key_encipherment_cert_sign_ku.pem",
 			ExpectedResult: lint.Error,
 		},
 		{
 			Name:           "Error - Certificate without digitalSignature or keyEncipherment KUs",
-			InputFilename:  "smime/rsa_strict_cert_sign_ku.pem",
+			InputFilename:  "smime/rsa_cert_sign_ku.pem",
 			ExpectedResult: lint.Error,
 		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.Name, func(t *testing.T) {
-			result := test.TestLint("e_rsa_key_usage_strict", tc.InputFilename)
+			result := test.TestLint("e_rsa_key_usage_legacy_multipurpose", tc.InputFilename)
 			if result.Status != tc.ExpectedResult {
 				t.Errorf("expected result %v was %v - details: %v", tc.ExpectedResult, result.Status, result.Details)
 			}
