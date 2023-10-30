2c1bbe48dd22951dbc952731cfadf74f34b49d24
BJ Cardon <cardonator@users.noreply.github.com>
2023-10-30 15:53:31 -0600

    added lints to check if the aia has likely internal names

---
+++ v3/lints/cabf_smime_br/lint_strict_aia_contains_internal_names_test.go
@@ -0,0 +1,35 @@
+package cabf_smime_br
+
+import (
+	"testing"
+
+	"github.com/zmap/zlint/v3/lint"
+	"github.com/zmap/zlint/v3/test"
+)
+
+func TestSMIMEStrictAIAInternalName(t *testing.T) {
+	testCases := []struct {
+		Name           string
+		InputFilename  string
+		ExpectedResult lint.LintStatus
+	}{
+		// {
+		// 	Name:           "pass - cert with SAN",
+		// 	InputFilename:  "smime/with_subject_alternative_name.pem",
+		// 	ExpectedResult: lint.Pass,
+		// },
+		{
+			Name:           "warn - cert with internal names in AIA",
+			InputFilename:  "smime/aiaWithInternalNamesLegacy.pem",
+			ExpectedResult: lint.Warn,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			result := test.TestLint("w_smime_strict_aia_contains_internal_names", tc.InputFilename)
+			if result.Status != tc.ExpectedResult {
+				t.Errorf("expected result %v was %v - details: %v", tc.ExpectedResult, result.Status, result.Details)
+			}
+		})
+	}
+}
