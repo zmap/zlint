ee42c94897261af9f4aa0dc7dc2348988fda77a2
Adam Bender <abender@gmail.com>
2023-12-13 14:34:04 -0800

    Add lints for S/MIME BR 7.1.2.3.b

---
+++ v3/lints/cabf_smime_br/lint_subscribers_crl_distribution_points_are_http.go
@@ -0,0 +1,78 @@
+/*
+ * ZLint Copyright 2023 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package cabf_smime_br
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/v3/lint"
+	"github.com/zmap/zlint/v3/util"
+)
+
+func init() {
+	lint.RegisterLint(&lint.Lint{
+		Name:          "e_subscribers_crl_distribution_points_are_http",
+		Description:   "cRLDistributionPoints SHALL have URL scheme HTTP.",
+		Citation:      "7.1.2.3.b",
+		Source:        lint.CABFSMIMEBaselineRequirements,
+		EffectiveDate: util.CABF_SMIME_BRs_1_0_0_Date,
+		Lint:          NewSubscriberCrlDistributionPointsHTTP,
+	})
+}
+
+type subscriberCrlDistributionPointsHTTP struct{}
+
+func NewSubscriberCrlDistributionPointsHTTP() lint.LintInterface {
+	return &subscriberCrlDistributionPointsHTTP{}
+}
+
+func (l *subscriberCrlDistributionPointsHTTP) CheckApplies(c *x509.Certificate) bool {
+	fmt.Println(util.IsSubscriberCert(c))
+	fmt.Println(util.IsMultipurposeSMIMECertificate(c))
+	fmt.Println(util.IsStrictSMIMECertificate(c))
+
+	b := util.IsSubscriberCert(c) && (util.IsMultipurposeSMIMECertificate(c) || util.IsStrictSMIMECertificate(c))
+
+	fmt.Printf("b = %t\n", b)
+	return b
+
+}
+
+func (l *subscriberCrlDistributionPointsHTTP) Execute(c *x509.Certificate) *lint.LintResult {
+	fmt.Println("exeucintg")
+	for _, dp := range c.CRLDistributionPoints {
+		fmt.Println(dp)
+
+		parsed, err := url.Parse(dp)
+		if err != nil {
+			return &lint.LintResult{
+				Status:  lint.Error,
+				Details: "SMIME certificate contains invalid CRL distribution point",
+			}
+		}
+
+		fmt.Printf("%+v\n", parsed.Scheme)
+
+		if parsed.Scheme != "http" {
+			return &lint.LintResult{
+				Status:  lint.Error,
+				Details: "SMIME certificate contains invalid URL scheme in CRL distribution point",
+			}
+		}
+	}
+	return &lint.LintResult{Status: lint.Pass}
+}
---
+++ v3/lints/cabf_smime_br/lint_subscribers_crl_distribution_points_are_http_test.go
@@ -0,0 +1,35 @@
+package cabf_smime_br
+
+import (
+	"testing"
+
+	"github.com/zmap/zlint/v3/lint"
+	"github.com/zmap/zlint/v3/test"
+)
+
+func TestSubscriberCrlDistributionPointsAreHTTP(t *testing.T) {
+	testCases := []struct {
+		Name           string
+		InputFilename  string
+		ExpectedResult lint.LintStatus
+	}{
+		{
+			Name:           "pass - cert with HTTP CRL distribution point",
+			InputFilename:  "smime/subscriber_with_http_crl_distribution_point.pem",
+			ExpectedResult: lint.Pass,
+		},
+		{
+			Name:           "error - cert without a non-HTTP CRL distribution point",
+			InputFilename:  "smime/subscriber_with_non_http_crl_distribution_point.pem",
+			ExpectedResult: lint.Error,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			result := test.TestLint("e_subscribers_crl_distribution_points_are_http", tc.InputFilename)
+			if result.Status != tc.ExpectedResult {
+				t.Errorf("expected result %v was %v - details: %v", tc.ExpectedResult, result.Status, result.Details)
+			}
+		})
+	}
+}
