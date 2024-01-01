be8dd6a629e36c9a9a34aeb7b34ed06327151ce3
Rob <3725956+robplee@users.noreply.github.com>
2024-01-01 18:15:24 +0000

    Limit e_registration_scheme_id_matches_subject_country to no longer apply to LEI or INT organizationIdentifiers (#781)
    * fix issue where e_registration_scheme_id_matches_subject_country was applying to LEI and INT certs where not required by the SMIME BRs
    
    * fix execution of e_registration_scheme_id_matches_subject_country lint in case where some organizationIdentifiers are subject to the check and others are not
    
    ---------
    
    Co-authored-by: Christopher Henderson <chris@chenderson.org>

--- v3/lints/cabf_smime_br/lint_registration_scheme_id_matches_subject_country.go
+++ v3/lints/cabf_smime_br/lint_registration_scheme_id_matches_subject_country.go
@@ -56,11 +56,18 @@ func (l *registrationSchemeIDMatchesSubjectCountry) CheckApplies(c *x509.Certifi
 		return false
 	}
 
+	orgIDsAreInternational := true
 	for _, id := range c.Subject.OrganizationIDs {
 		submatches := countryRegex.FindStringSubmatch(id)
 		if len(submatches) < 3 {
 			return false
 		}
+
+		orgIDsAreInternational = orgIDsAreInternational && (submatches[1] == "INT" || submatches[1] == "LEI")
+	}
+
+	if orgIDsAreInternational {
+		return false
 	}
 
 	return util.IsOrganizationValidatedCertificate(c) || util.IsSponsorValidatedCertificate(c)
@@ -81,6 +88,11 @@ func (l *registrationSchemeIDMatchesSubjectCountry) Execute(c *x509.Certificate)
 // verifySMIMEOrganizationIdentifierContainSubjectNameCountry verifies that the country code used in the subject:organizationIdentifier matches subject:countryName
 func verifySMIMEOrganizationIdentifierContainsSubjectNameCountry(id string, country string) error {
 	submatches := countryRegex.FindStringSubmatch(id)
+
+	if submatches[1] == "INT" || submatches[1] == "LEI" {
+		return nil
+	}
+
 	// Captures the country code from the organization identifier
 	// Note that this raw indexing into the second position is only safe
 	// due to a length check done in CheckApplies
