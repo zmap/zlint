69caf00cf041153afca2ad48139cab2ab999f81f
mtgag <githreg@mtg.de>
2024-01-24 07:58:04 +0100

    if the AIA contains an IP then pass instead of warn

--- v3/lints/cabf_br/lint_sub_cert_aia_contains_internal_names.go
+++ v3/lints/cabf_br/lint_sub_cert_aia_contains_internal_names.go
@@ -15,6 +15,7 @@ package cabf_br
  */
 
 import (
+	"net"
 	"net/url"
 	"time"
 
@@ -62,6 +63,11 @@ func (l *subCertAIAInternalName) Execute(c *x509.Certificate) *lint.LintResult {
 		if err != nil {
 			return &lint.LintResult{Status: lint.Error}
 		}
+
+		if net.ParseIP(purl.Host) != nil {
+			continue
+		}
+
 		if !util.HasValidTLD(purl.Hostname(), time.Now()) {
 			return &lint.LintResult{Status: lint.Warn}
 		}
@@ -71,6 +77,11 @@ func (l *subCertAIAInternalName) Execute(c *x509.Certificate) *lint.LintResult {
 		if err != nil {
 			return &lint.LintResult{Status: lint.Error}
 		}
+
+		if net.ParseIP(purl.Host) != nil {
+			continue
+		}
+
 		if !util.HasValidTLD(purl.Hostname(), time.Now()) {
 			return &lint.LintResult{Status: lint.Warn}
 		}
--- v3/lints/cabf_br/lint_sub_cert_aia_contains_internal_names_test.go
+++ v3/lints/cabf_br/lint_sub_cert_aia_contains_internal_names_test.go
@@ -23,6 +23,11 @@ func TestAIAInternalName(t *testing.T) {
 			InputFilename:  "aiaWithInternalNames.pem",
 			ExpectedResult: lint.Warn,
 		},
+		{
+			Name:           "pass - aia with an IP address",
+			InputFilename:  "aiaWithIP.pem",
+			ExpectedResult: lint.Pass,
+		},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.Name, func(t *testing.T) {
