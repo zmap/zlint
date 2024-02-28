75274a9832bce35b118f1a0cad1846ebd0d4416b
Adam Bender <abender@gmail.com>
2024-02-28 13:05:15 -0800

    Add lint for BR 7.1.4.2.2a mailbox-validated

---
+++ v3/lints/cabf_smime_br/lint_commonname_mailbox_validated.go
@@ -0,0 +1,79 @@
+/*
+ * ZLint Copyright 2024 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package cabf_smime_br
+
+import (
+	"fmt"
+
+	"github.com/zmap/zcrypto/encoding/asn1"
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/v3/lint"
+	"github.com/zmap/zlint/v3/util"
+)
+
+func init() {
+	lint.RegisterCertificateLint(&lint.CertificateLint{
+		LintMetadata: lint.LintMetadata{
+			Name:          "e_commonname_mailbox_validated",
+			Description:   "If present, the commonName attribute SHALL contain one of the following values",
+			Citation:      "S/MIME BRs: 7.1.4.2.2a",
+			Source:        lint.CABFSMIMEBaselineRequirements,
+			EffectiveDate: util.CABF_SMIME_BRs_1_0_0_Date,
+		},
+		Lint: NewCommonNameMailboxValidated,
+	})
+}
+
+type commonNameMailboxValidated struct{}
+
+func NewCommonNameMailboxValidated() lint.LintInterface {
+	return &commonNameMailboxValidated{}
+}
+
+func (l *commonNameMailboxValidated) CheckApplies(c *x509.Certificate) bool {
+	return util.IsMailboxValidatedCertificate(c)
+}
+
+func (l *commonNameMailboxValidated) Execute(c *x509.Certificate) *lint.LintResult {
+	rdnSequence := util.RawRDNSequence{}
+	rest, err := asn1.Unmarshal(c.RawSubject, &rdnSequence)
+	if err != nil {
+		return &lint.LintResult{
+			Status:  lint.Fatal,
+			Details: "Failed to Unmarshal RawSubject into RawRDNSequence",
+		}
+	}
+	if len(rest) > 0 {
+		return &lint.LintResult{
+			Status:  lint.Fatal,
+			Details: "Trailing data after RawSubject RawRDNSequence",
+		}
+	}
+
+	for _, attrTypeAndValueSet := range rdnSequence {
+		for _, attrTypeAndValue := range attrTypeAndValueSet {
+			fmt.Printf("%+v\n", attrTypeAndValue)
+		}
+	}
+
+	commonNames := []string{c.Subject.CommonName}
+	commonNames = append(commonNames, c.Subject.CommonNames...)
+	for _, cn := range commonNames {
+		if !util.IsMailboxAddress(cn) {
+			return &lint.LintResult{Status: lint.Error}
+		}
+	}
+	return &lint.LintResult{Status: lint.Pass}
+}
