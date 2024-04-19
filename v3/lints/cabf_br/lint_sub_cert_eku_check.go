4e27fe51cd308d7ba68e941c91602ed6079f89db
Paul van Brouwershaven <vanbroup@users.noreply.github.com>
2024-04-19 20:32:37 +0200

    Check if subscriber certificate with EKU extension

--- v3/lints/cabf_br/lint_sub_cert_eku_check.go
+++ v3/lints/cabf_br/lint_sub_cert_eku_check.go
@@ -42,7 +42,7 @@ func NewSubExtKeyUsageCheck() lint.LintInterface {
 }
 
 func (l *subExtKeyUsageCheck) CheckApplies(c *x509.Certificate) bool {
-	return c.ExtKeyUsage != nil
+	return util.IsSubscriberCert() && util.IsExtInCert(c, util.EkuSynOid)
 }
 
 func (l *subExtKeyUsageCheck) Execute(c *x509.Certificate) *lint.LintResult {
