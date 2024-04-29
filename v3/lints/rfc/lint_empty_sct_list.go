6f032d2ea6885825502a36b9cda2930f1108b94e
Adriano Santoni <asantoni64@gmail.com>
2024-04-29 13:27:08 +0200

    Add files via upload

--- v3/lints/rfc/lint_empty_sct_list.go
+++ v3/lints/rfc/lint_empty_sct_list.go
@@ -11,8 +11,8 @@
  * implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
- 
- /*
+
+/*
  * Contributed by Adriano Santoni <asantoni64@gmail.com>
  */
 
