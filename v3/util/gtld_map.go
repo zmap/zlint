08ea12865f6f194ae21e316ba44b2530c68b7ece
GitHub <noreply@github.com>
2024-06-12 22:20:11 +0000

    util: gtld_map autopull updates for 2024-06-12T22:19:30 UTC

--- v3/util/gtld_map.go
+++ v3/util/gtld_map.go
@@ -4416,7 +4416,7 @@ var tldMap = map[string]GTLDPeriod{
 	"natura": {
 		GTLD:           "natura",
 		DelegationDate: "2016-02-11",
-		RemovalDate:    "",
+		RemovalDate:    "2024-06-12",
 	},
 	"navy": {
 		GTLD:           "navy",
