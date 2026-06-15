5bdf5f729435bc6c594bf2ab06574b5e9ba15881
Ren Peterson <ren.peterson@digicert.com>
2026-06-14 23:01:48 -0600

    Fix comment and references

--- v3/lints/cabf_cs_br/lint_cs_subject_locality_or_province.go
+++ v3/lints/cabf_cs_br/lint_cs_subject_locality_or_province.go
@@ -8,13 +8,18 @@ import (
 
 /*
 7.1.4.2.3 Subject distinguished name field - Non-EV Code Signing Certificates
-g. Certificate Field: subject:localityName (OID: 2.5.4.7)
-Required/Optional: Required if the subject:stateOrProvinceName field is absent. Optional if
-the subject:stateOrProvinceName field is present.
+c. Certificate Field: subject:localityName (OID: 2.5.4.7)
+Required/Optional: Required if the subject:stateOrProvinceName field is absent. Optional if the subject:stateOrProvinceName field is present.
+Contents: If present, the subject:localityName field MUST contain the Subject’s locality information as verified under BR Section 3.2.
+If the subject:countryName field specifies the ISO 3166-1 user-assigned code of XX in accordance with BR Section 7.1.4.2.2.h., the
+subject:localityName field MAY contain the Subject’s locality and/or state or province information as verified under BR Section 3.2.2.1 or 3.2.3.
 
-h. Certificate Field: subject:stateOrProvinceName (OID: 2.5.4.8)
-Required/Optional: Required if the subject:localityName field is absent. Optional if
-the subject:localityName field is present.
+d. Certificate Field: subject:stateOrProvinceName (OID: 2.5.4.8)
+Required/Optional: Required if the subject:localityName field is absent. Optional if the subject:localityName field is present.
+Contents: If present, the subject:stateOrProvinceName field MUST contain the Subject’s state or province information as verified
+under BR Section 3.2.2.1 or 3.2.3. If the subject:countryName field specifies the ISO 3166-1 user-assigned code of XX in accordance with
+BR Section 7.1.4.2.2.h., the subject:stateOrProvinceName field MAY contain the full name of the Subject’s country information as verified
+under BR Section 3.2.2.1 or 3.2.3.
 */
 
 func init() {
@@ -22,7 +27,7 @@ func init() {
 		LintMetadata: lint.LintMetadata{
 			Name:          "e_cs_subject_locality_or_province_required",
 			Description:   "Non-EV Code Signing Certificate: at least one of subject:localityName or subject:stateOrProvinceName must be present.",
-			Citation:      "CABF CS BRs 7.1.4.2.3.g, 7.1.4.2.3.h",
+			Citation:      "CABF CS BRs 7.1.4.2.3.c, 7.1.4.2.3.d",
 			Source:        lint.CABFCSBaselineRequirements,
 			EffectiveDate: util.CABF_CS_BRs_1_2_Date,
 		},
