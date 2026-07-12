df744856c2d987d85c437c3590565788da6a9df6
Adriano Santoni <asantoni64@gmail.com>
2026-07-12 17:31:05 +0200

    Add lint for duplicate ETSI-defined qCStatements (#1060)
    * Add files via upload
    
    * Add files via upload
    
    * Add EtsiEn319_412_5_V2_6_1_Date constant
    
    ---------
    
    Co-authored-by: Christopher Henderson <chris@chenderson.org>

--- v3/util/time.go
+++ v3/util/time.go
@@ -67,6 +67,7 @@ var (
 	EtsiEn319_412_5_V2_2_1_Date                      = time.Date(2017, time.November, 1, 0, 0, 0, 0, time.UTC)
 	EtsiEn319_412_5_V2_4_1_Date                      = time.Date(2023, time.September, 1, 0, 0, 0, 0, time.UTC)
 	EtsiEn319_412_5_V2_6_0_Date                      = time.Date(2026, time.February, 1, 0, 0, 0, 0, time.UTC)
+	EtsiEn319_412_5_V2_6_1_Date                      = time.Date(2026, time.May, 1, 0, 0, 0, 0, time.UTC)
 	EtsiEn319_411_2_V2_5_0_Date                      = time.Date(2023, time.July, 1, 0, 0, 0, 0, time.UTC)
 	OnionOnlyEVDate                                  = time.Date(2015, time.May, 1, 0, 0, 0, 0, time.UTC)
 	CABV201Date                                      = time.Date(2017, time.July, 28, 0, 0, 0, 0, time.UTC)
