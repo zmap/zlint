1007b1d55fa0de8d6639088d6a7a6493b09b858c
Adriano Santoni <asantoni64@gmail.com>
2026-08-02 22:03:47 +0200

    Add lint to check for a valid commonName in Organization Validated S/MIME certificates (#1069)
    * Add files via upload
    
    * Add files via upload
    
    ---------
    
    Co-authored-by: Christopher Henderson <chris@chenderson.org>

---
+++ v3/lints/cabf_smime_br/lint_org_validated_invalid_cn.go
@@ -0,0 +1,67 @@
+/*
+ * ZLint Copyright 2024 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package cabf_smime_br
+
+import (
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/v3/lint"
+	"github.com/zmap/zlint/v3/util"
+
+	"net/mail"
+)
+
+func init() {
+	lint.RegisterCertificateLint(&lint.CertificateLint{
+		LintMetadata: lint.LintMetadata{
+			Name:          "e_org_validated_invalid_cn",
+			Description:   "In OV S/MIME certs, the Subject CN must either contain an email address or match organizatioName",
+			Citation:      "CABF SMIME BRs §7.1.4.2.2",
+			Source:        lint.CABFSMIMEBaselineRequirements,
+			EffectiveDate: util.CABF_SMIME_BRs_1_0_0_Date,
+		},
+		Lint: NewOrgValidatedInvalidCN,
+	})
+}
+
+type OrgValidatedInvalidCN struct{}
+
+func NewOrgValidatedInvalidCN() lint.LintInterface {
+	return &OrgValidatedInvalidCN{}
+}
+
+func (l *OrgValidatedInvalidCN) CheckApplies(c *x509.Certificate) bool {
+	return util.IsSubscriberCert(c) && util.IsOrganizationValidatedCertificate(c)
+}
+
+func isEmail(s string) bool {
+	addr, err := mail.ParseAddress(s)
+	if err != nil {
+		return false
+	}
+	return addr.Address == s
+}
+
+func (l *OrgValidatedInvalidCN) Execute(c *x509.Certificate) *lint.LintResult {
+
+	if isEmail(c.Subject.CommonName) ||
+		c.Subject.CommonName == c.Subject.Organization[0] {
+		return &lint.LintResult{Status: lint.Pass}
+	}
+
+	return &lint.LintResult{
+		Status:  lint.Error,
+		Details: "In OV S/MIME certs, the Subject CN must either contain an email address or match organizatioName",
+	}
+}
---
+++ v3/lints/cabf_smime_br/lint_org_validated_invalid_cn_test.go
@@ -0,0 +1,76 @@
+/*
+ * ZLint Copyright 2024 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package cabf_smime_br
+
+import (
+	"testing"
+
+	"github.com/zmap/zlint/v3/lint"
+	"github.com/zmap/zlint/v3/test"
+)
+
+func TestOrgValidatedInvalidCN(t *testing.T) {
+
+	testCases := []struct {
+		desc string
+		path string
+		want lint.LintStatus
+	}{
+		{
+			desc: "CA certificate",
+			path: "smime/sub0_smx_ovx_cnex_cnox_effx.pem",
+			want: lint.NA,
+		},
+		{
+			desc: "Non-S/MIME certificate",
+			path: "smime/sub1_sm0_ovx_cnex_cnox_effx.pem",
+			want: lint.NA,
+		},
+		{
+			desc: "Non-OV S/MIME certificate",
+			path: "smime/sub1_sm1_ov0_cnex_cnox_effx.pem",
+			want: lint.NA,
+		},
+		{
+			desc: "OV S/MIME certificate with email in CN",
+			path: "smime/sub1_sm1_ov1_cne1_cno0_eff1.pem",
+			want: lint.Pass,
+		},
+		{
+			desc: "OV S/MIME certificate with CN matching O",
+			path: "smime/sub1_sm1_ov1_cne0_cno1_eff1.pem",
+			want: lint.Pass,
+		},
+		{
+			desc: "OV S/MIME certificate with bad CN, issued before effective date",
+			path: "smime/sub1_sm1_ov1_cne0_cno0_eff0.pem",
+			want: lint.NE,
+		},
+		{
+			desc: "OV S/MIME certificate with bad CN, issued after effective date",
+			path: "smime/sub1_sm1_ov1_cne0_cno0_eff1.pem",
+			want: lint.Error,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			out := test.TestLint("e_org_validated_invalid_cn", tc.path)
+			if out.Status != tc.want {
+				t.Errorf("expected status %s for %s, got %s", tc.want, tc.path, out.Status)
+			}
+		})
+	}
+}
