45e58aed1691c458bc71085a27cb94aa8a1750bd
christopher-henderson <chris@chenderson.org>
2026-08-08 19:02:18 -0700

    lints/etsi: add e_qcstatem_psd2_ncaname_valid

---
+++ v3/lints/etsi/lint_qcstatem_psd2_ncaname_valid.go
@@ -0,0 +1,77 @@
+/*
+ * ZLint Copyright 2026 Regents of the University of Michigan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package etsi
+
+import (
+	"unicode/utf8"
+
+	"github.com/zmap/zcrypto/x509"
+	"github.com/zmap/zlint/v3/lint"
+	"github.com/zmap/zlint/v3/util"
+)
+
+type qcStatemPsd2NcaNameValid struct{}
+
+func init() {
+	lint.RegisterCertificateLint(&lint.CertificateLint{
+		LintMetadata: lint.LintMetadata{
+			Name:          "e_qcstatem_psd2_ncaname_valid",
+			Description:   "Checks that the NCAName field of a PSD2 QcStatement is non-empty and at most 256 characters, or 'NA' for a certificate declaring a PSP_CB or PSP_PA role",
+			Citation:      "ETSI TS 119 495 V1.1.2 (2018-07), Section 5.2.3, GEN-5.2.3-1",
+			Source:        lint.EtsiEsi,
+			EffectiveDate: util.EtsiTs119495_V1_1_2_Date,
+		},
+		Lint: NewQcStatemPsd2NcaNameValid,
+	})
+}
+
+func NewQcStatemPsd2NcaNameValid() lint.LintInterface {
+	return &qcStatemPsd2NcaNameValid{}
+}
+
+func (l *qcStatemPsd2NcaNameValid) CheckApplies(c *x509.Certificate) bool {
+	if !util.IsExtInCert(c, util.QcStateOid) {
+		return false
+	}
+	return util.ParseQcStatem(util.GetExtFromCert(c, util.QcStateOid).Value, util.IdEtsiPsd2Statem).IsPresent()
+}
+
+func (l *qcStatemPsd2NcaNameValid) Execute(c *x509.Certificate) *lint.LintResult {
+	ext := util.GetExtFromCert(c, util.QcStateOid)
+	s := util.ParseQcStatem(ext.Value, util.IdEtsiPsd2Statem)
+	if s.GetErrorInfo() != "" {
+		return &lint.LintResult{Status: lint.Error, Details: s.GetErrorInfo()}
+	}
+	psd2, ok := s.(util.EtsiPsd2)
+	if !ok {
+		return &lint.LintResult{Status: lint.Fatal, Details: "parsed QC statement is not of type EtsiPsd2"}
+	}
+
+	if util.IsPsd2CentralBankOrPublicAuthority(psd2.Decoded.RolesOfPSP) {
+		if psd2.Decoded.NCAName != "NA" {
+			return &lint.LintResult{Status: lint.Error, Details: "NCAName must be 'NA' for a PSD2 QcStatement declaring a PSP_CB or PSP_PA role"}
+		}
+		return &lint.LintResult{Status: lint.Pass}
+	}
+
+	nameLen := utf8.RuneCountInString(psd2.Decoded.NCAName)
+	if nameLen == 0 {
+		return &lint.LintResult{Status: lint.Error, Details: "NCAName must not be empty"}
+	}
+	if nameLen > 256 {
+		return &lint.LintResult{Status: lint.Error, Details: "NCAName must be at most 256 characters"}
+	}
+	return &lint.LintResult{Status: lint.Pass}
+}
--- v3/util/qc_stmt.go
+++ v3/util/qc_stmt.go
@@ -115,6 +115,18 @@ type EtsiPsd2 struct {
 	Decoded PSD2QcType
 }
 
+// IsPsd2CentralBankOrPublicAuthority reports whether roles declares a PSP_CB
+// (Central Bank) or PSP_PA (Public authority) role, per ETSI TS 119 495
+// Annex A. Certificates declaring either role are subject to the
+// GEN-5.2.3-1A/GEN-5.2.3-5 "NA" carve-out for NCAName/NCAId.
+func IsPsd2CentralBankOrPublicAuthority(roles []RoleOfPSP) bool {
+	oids := make([]asn1.ObjectIdentifier, 0, len(roles))
+	for _, role := range roles {
+		oids = append(oids, role.RoleOfPspOid)
+	}
+	return SliceContainsOID(oids, IdEtsiPsd2RolePspCb) || SliceContainsOID(oids, IdEtsiPsd2RolePspPa)
+}
+
 func AppendToStringSemicolonDelim(this *string, s string) {
 	if len(*this) > 0 && len(s) > 0 {
 		(*this) += "; "
